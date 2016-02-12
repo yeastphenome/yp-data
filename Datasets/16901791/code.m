0232578d820496995d9390d3f4ee8611318a2ad2
Brianna Richardson <bricha2@umbc.edu>
2016-02-12 16:38:33 -0500

    1/12/16 Update
    Parsons~Boone, 2006

---
+++ Datasets/16901791/code.m
@@ -0,0 +1,57 @@
+%% Parsons~Boone, 2015
+function FILENAMES = code()
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+FILENAMES = {};
+parsons_boone_2006.pmid = 16901791;
+
+phenotypes = {'sensitivity'};
+
+%% Hit Strains
+
+% Load hit strains and tested strains
+[FILENAMES{end+1}, data] = readdata('xlsread', './raw_data/mmc8.xlsx', 'Supplementary Table 7');
+
+% Get list of ORFs from both sets of ORFs
+hit_orfs = data(4:end,1);
+
+% Get Treatments
+treatments = data(3, 2:end);
+
+% Get rid of empty rows
+inds = find(cellfun(@isnumeric, hit_orfs));
+hit_orfs(inds) = [];
+
+% Clean up ORFs
+hit_orfs = cleanOrf(hit_orfs);
+
+% Find anything that doesn't look like an ORF
+inds = find(~isorf(hit_orfs));
+disp(hit_orfs(inds)); 
+
+% Make sure all are unique
+hit_orfs = unique(hit_orfs);
+
+% Retrieve data
+hit_data = data(4:end, 2:end);
+
+% Make a matrix of NaNs
+inds = find(~cellfun(@isnumeric, hit_data));
+hit_data(inds) = {nan};
+hit_data = cell2mat(hit_data);
+
+% Data Calculation.
+hit_data = hit_data ./ -1;
+
+% Average any repeated value
+[hit_orfs, hit_data] = grpstats(hit_data, hit_orfs, {'gname','mean'});
+
+% Prepare final dataset
+parsons_boone_2006.orfs = hit_orfs;
+parsons_boone_2006.ph = strcat(phenotypes, '; ', treatments);
+parsons_boone_2006.data = hit_data;
+
+%% Save
+
+save('./parsons_boone_2006.mat','parsons_boone_2006');
+
+end
