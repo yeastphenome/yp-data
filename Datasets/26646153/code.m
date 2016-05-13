e3c522737da46062d28694f5d3f6b30a933d023d
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-05-13 14:03:50 -0400

    Fixed bugs in code and naming

--- Datasets/26646153/code.m
+++ Datasets/26646153/code.m
@@ -2,53 +2,37 @@
 function FILENAMES = code()
 addpath(genpath('../../Yeast-Matlab-Utils/'));
 FILENAMES = {};
-choi_basrai_2015.pmid = 26646153;
+choy_basrai_2015.pmid = 26646153;
 
-phenotypes = {'growth'};
-treatment = {'nicotinamide, 120 mM'};
+hit_data_ids = [768];
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(choy_basrai_2015.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
 
 %% Hit Strains
 
 % Load file from first test
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P1');
-all_data = data;
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P2');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P3');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P4');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P5');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P6');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P7');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P8');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P9');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P10');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P11');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P12');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P13');
-all_data = [all_data; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', 'S1 P14');
-all_data = [all_data; data];
+all_data = [];
+for p = 1 : 14
+    [FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS2.xlsx', ['S1 P' num2str(p)]);
+    all_data = [all_data; data];
+end
 
 % Get the list of ORFs
 strains = all_data(:,1);
+
+% Clean up ORFs
+strains = clean_orf(strains);
 
 % Find anything that doesn't look like an ORF and remove it
 inds = find(~is_orf(strains));
 all_data(inds, :) = [];
 strains(inds, :) = [];
-
-% Clean up ORFs
-strains = clean_orf(strains);
 
 % Get data from hits
 hit_data = all_data(:,6);
@@ -57,45 +41,22 @@ hit_data(indx) = {NaN};
 hit_data = cell2mat(hit_data);
 
 % Load file from second test
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P1');
-all_data2 = data;
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P2');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P3');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P4');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P5');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P6');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P7');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P8');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P9');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P10');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P11');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P12');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P13');
-all_data2 = [all_data2; data];
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', 'S2 P14');
-all_data2 = [all_data2; data];
+all_data2 = [];
+for p = 1 : 14
+    [FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/supp_g3.115.022244_TableS3.xlsx', ['S2 P' num2str(p)]);
+    all_data2 = [all_data2; data];
+end
 
 % Get the list of ORFs
 strains2 = all_data2(:,1);
+
+% Clean up ORFs
+strains2 = clean_orf(strains2);
 
 % Find anything that doesn't look like an ORF and remove it
 inds = find(~is_orf(strains2));
 all_data2(inds, :) = [];
 strains2(inds, :) = [];
-
-% Clean up ORFs
-strains2 = clean_orf(strains2);
 
 % Get data from hits
 hit_data2 = all_data2(:,6);
@@ -104,25 +65,41 @@ hit_data2(indx) = {NaN};
 hit_data2 = cell2mat(hit_data2);
 
 % Combine the data and find the average
-final_data = [hit_data hit_data2];
-final_data = sum(final_data, 2);
+[strains, hit_data] = grpstats(hit_data, strains, {'gname','mean'});
+[strains2, hit_data2] = grpstats(hit_data2, strains2, {'gname','mean'});
+
+final_strains = unique([strains; strains2]);
+final_data = nan(length(final_strains),2);
+
+[~,ind1,ind2] = intersect(strains, final_strains);
+final_data(ind2,1) = hit_data(ind1);
+
+[~,ind1,ind2] = intersect(strains2, final_strains);
+final_data(ind2,2) = hit_data2(ind1);
+
+final_data = nanmean(final_data, 2);
+
 final_data = 1 ./ final_data;
-final_strains = strains;
 
-% Average any repeated value
-[final_strains, final_data] = grpstats(final_data, final_strains, {'gname','mean'});
+%% Prepare final dataset
 
-% Prepare final dataset
-choi_basrai_2015.orfs = final_strains;
-choi_basrai_2015.ph = strcat(phenotypes, '; ', treatment);
-choi_basrai_2015.data = final_data;
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+
+choy_basrai_2015.orfs = final_strains;
+choy_basrai_2015.ph = hit_data_names;
+choy_basrai_2015.data = final_data;
+choy_basrai_2015.dataset_ids = hit_dataset_ids;
 
 %% Save
 
-save('./choi_basrai_2015.mat','choi_basrai_2015');
+save('./choy_basrai_2015.mat','choy_basrai_2015');
 
-fid = fopen('./choi_basrai_2015.txt','w');
-write_matrix_file(fid, choi_basrai_2015.orfs, choi_basrai_2015.ph, choi_basrai_2015.data);
+fid = fopen('./choy_basrai_2015.txt','w');
+write_matrix_file(fid, choy_basrai_2015.orfs, choy_basrai_2015.ph, choy_basrai_2015.data);
 fclose(fid);
 
 end
