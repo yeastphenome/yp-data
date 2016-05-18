5413048185a4ce852ccf0a7d8ecde982bb617443
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-05-18 13:57:59 -0400

    Added dataset_ids to PMID 16901791

--- Datasets/16901791/code.m
+++ Datasets/16901791/code.m
@@ -4,7 +4,13 @@ addpath(genpath('../../Yeast-Matlab-Utils/'));
 FILENAMES = {};
 parsons_boone_2006.pmid = 16901791;
 
-phenotypes = {'sensitivity'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(parsons_boone_2006.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
 
 %% Hit Strains
 
@@ -15,7 +21,7 @@ phenotypes = {'sensitivity'};
 hit_orfs = data(4:end,1);
 
 % Get Treatments
-treatments = data(3, 2:end);
+treatments = strtrim(data(3, 2:end));
 
 % Get rid of empty rows
 inds = find(cellfun(@isnumeric, hit_orfs));
@@ -28,27 +34,43 @@ hit_orfs = clean_orf(hit_orfs);
 inds = find(~is_orf(hit_orfs));
 disp(hit_orfs(inds)); 
 
-% Make sure all are unique
-hit_orfs = unique(hit_orfs);
-
 % Retrieve data
 hit_data = data(4:end, 2:end);
 
-% Make a matrix of NaNs
 inds = find(~cellfun(@isnumeric, hit_data));
-hit_data(inds) = {nan};
+hit_data(inds) = {NaN};
 hit_data = cell2mat(hit_data);
 
 % Data Calculation.
-hit_data = hit_data ./ -1;
+hit_data = -hit_data;
 
 % Average any repeated value
 [hit_orfs, hit_data] = grpstats(hit_data, hit_orfs, {'gname','mean'});
 
-% Prepare final dataset
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+[FILENAMES{end+1}, dataset_map] = read_data('readtable', './extras/phenotype_dataset.txt', 'Delimiter','\t', 'ReadVariableNames', false);
+
+[~,ind1,ind2] = intersect(treatments, strtrim(dataset_map.Var1));
+hit_data_ids = nan(size(treatments));
+hit_data_ids(ind1) = dataset_map.Var2(ind2);
+
+[hit_data_ids, hit_data] = grpstats(hit_data', hit_data_ids, {'gname','mean'});
+hit_data = hit_data';
+hit_data_ids = cellfun(@str2num, hit_data_ids);
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
 parsons_boone_2006.orfs = hit_orfs;
-parsons_boone_2006.ph = strcat(phenotypes, '; ', treatments);
+parsons_boone_2006.ph = hit_data_names;
 parsons_boone_2006.data = hit_data;
+parsons_boone_2006.dataset_ids = hit_data_ids; 
 
 %% Save
 
