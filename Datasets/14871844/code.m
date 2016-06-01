73f703221479688ab36ed0702354269ef746f1f2
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-06-01 17:22:43 -0400

    Updated dataset ids in PMID 14871844

--- Datasets/14871844/code.m
+++ Datasets/14871844/code.m
@@ -6,27 +6,43 @@ addpath(genpath('../../Yeast-Matlab-Utils/'));
 FILENAMES = {};
 aouida_ramotar_2004.pmid = 14871844;
 
-phenotypes = {'growth'};
-treatments = {'bleomycin'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
 
-% Load tested
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(aouida_ramotar_2004.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load tested
+
 [FILENAMES{end+1}, tested.raw] = read_data('xlsread','./raw_data/HU haploid.xlsx');
 tested_orfs = tested.raw(4:end,5);
+
+% Eliminate all white spaces & capitalize
+tested_orfs = clean_orf(tested_orfs);
 
 inds = find(cellfun(@isempty, tested_orfs) | cellfun(@isnumeric, tested_orfs));
 tested_orfs(inds) = [];
 
-tested_orfs = unique(strtrim(upper(tested_orfs)));
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(tested_orfs));
+disp(tested_orfs(inds));  
 
-inds = find(~strncmp('Y', tested_orfs,1));
-tested_orfs(inds) = [];
+tested_orfs = unique(tested_orfs);
 
-% Load data
+
+%% Load data
+
 [FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/CAN_2-1-04_Aouida.xlsx');
 hits_orfs = data.raw(:,1);
-hits_orfs = strtrim(upper(hits_orfs));
 
+% Eliminate all white spaces & capitalize
+hits_orfs = clean_orf(hits_orfs);
 
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hits_orfs));
+disp(hits_orfs(inds));
 
 hits_data = strtrim(data.raw(:,2));
 
@@ -42,17 +58,31 @@ hits_data = cell2mat(hits_data);
 
 [missing, ix] = setdiff(hits_orfs, tested_orfs);
 
-inds = find(~strncmp('Y', hits_orfs,1));
-hits_orfs(inds) = [];
+%%
 
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [96];
 
+%%
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is discrete/binary and the tested strains were provided separately:
 aouida_ramotar_2004.orfs = tested_orfs;
-aouida_ramotar_2004.data = zeros(length(tested_orfs),1);
+aouida_ramotar_2004.ph = hit_data_names;
+aouida_ramotar_2004.data = zeros(length(aouida_ramotar_2004.orfs),length(aouida_ramotar_2004.ph));
+aouida_ramotar_2004.dataset_ids = hit_data_ids;
 
-[~,ind1,ind2] = intersect(hits_orfs, tested_orfs);
-aouida_ramotar_2004.data(ind2) = hits_data(ind1);
+[~,ind1,ind2] = intersect(hits_orfs, aouida_ramotar_2004.orfs);
+aouida_ramotar_2004.data(ind2,:) = hits_data(ind1,:);
 
-aouida_ramotar_2004.ph = strcat(phenotypes, '; ', treatments);
 
 save('./aouida_ramotar_2004.mat','aouida_ramotar_2004');
 
