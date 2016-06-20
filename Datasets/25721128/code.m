b469cdcf5d1bc4295a89798945be2dc5ded88086
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-06-20 16:52:13 -0400

    Updated code for Hendry~Brown, 2015

--- Datasets/25721128/code.m
+++ Datasets/25721128/code.m
@@ -7,6 +7,14 @@ hendry_brown_2015.pmid = 25721128;
 phenotypes = {'Rnr3 abundance'};
 treatments = {'Untreated conditions'; 'YPD, 0.03% MMS'};
 
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(hendry_brown_2015.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
 %% Hit Strains
 
 % Load hit strains
@@ -28,26 +36,40 @@ inds = find(~is_orf(hit_strains2));
 disp(hit_strains2(inds));
 
 % Retrieve data from files
-hit_data = data(11:end, 8);
-hit_data2 = data2(11:end, 8);
+hit_data = cell2mat(data(11:end, 8));
+hit_data2 = cell2mat(data2(11:end, 8));
 
 % Average any repeated value
 [hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
 [hit_strains2, hit_data2] = grpstats(hit_data2, hit_strains2, {'gname','mean'});
 
-
-% Retrieve data from individual files and put them in the data
+% Combined the 2 datasets
 hit_strains3 = unique([hit_strains; hit_strains2]);
 hit_data3 = nan(length(hit_strains3),2);
+
 [~,ind1,ind2] = intersect(hit_strains3, hit_strains);
-hit_data3(ind1,1) = cell2mat(hit_data(ind2));
+hit_data3(ind1,1) = hit_data(ind2);
+
 [~,ind1,ind2] = intersect(hit_strains3, hit_strains2);
 hit_data3(ind1,2) = hit_data2(ind2);
 
-% Prepare final dataset
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [693;606];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
 hendry_brown_2015.orfs = hit_strains3;
-hendry_brown_2015.ph = strcat(phenotypes, '; ', treatments);
+hendry_brown_2015.ph = hit_data_names;
 hendry_brown_2015.data = hit_data3;
+hendry_brown_2015.dataset_ids = hit_data_ids;
+
 
 %% Save
 
