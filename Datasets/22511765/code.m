d4e9424d5a9897f5f0ed5e3d48d7ae02183cb3dd
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-06-21 14:01:49 -0400

    Updated code & data for Kim~Cunningham, 2012

--- Datasets/22511765/code.m
+++ Datasets/22511765/code.m
@@ -4,42 +4,59 @@ function FILENAMES = code()
 addpath(genpath('../../Yeast-Matlab-Utils/'));
 
 FILENAMES = {};
-
 kim_cunningham_2012.pmid = 22511765;
 
-phenotypes = {'Cell death, frequency of dead cells'};
-treatments = {'tunicamycin, 2.5 ug/ml (Z-score ln)'; 'tunicamycin + FK506, 2.5 ug/ml + 1 ug/ml (Z-score ln)'; 'dithiothreitol, 4 mM (Z-score ln)'; 'dithiothreitol, 4 mM, + FK506, 1 ug/ml (Z-score ln)'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(kim_cunningham_2012.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%%
 
 [FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/jbc.M112.363390-1.xlsx', 'sup table 1');
 
-% Get indices of the data columns
-ind_data = [14:15 17:18];
+% Get ORFs & data
+hit_orfs = data.raw(3:end, 5);
+hit_data = data.raw(3:end, 14:15);
 
 % Eliminate anything that doesn't look like an ORF
-inds = find(cellfun(@isnumeric, data.raw(:,5)));
-data.raw(inds,:) = [];
+hit_orfs = clean_orf(hit_orfs);
 
-inds = setdiff(1:length(data.raw(:,5)),strmatch('Y', data.raw(:,5)));
-data.raw(inds,:) = [];
+hit_orfs(strcmp('YLR287-A', hit_orfs)) = {'YLR287C-A'};
+inds = find(~is_orf(hit_orfs));
+hit_orfs(inds) = [];
+hit_data(inds,:) = [];
 
-% Eliminate white spaces before/after ORF
-data.raw(:,5) = cellfun(@strtrim, data.raw(:,5),'UniformOutput',0);
-
-data2.orfs = upper(data.raw(:,5));
-data2.data = data.raw(:,ind_data);
-
-% Make sure all the data are numbers
-inds = find(cellfun(@isnumeric, data2.data)==0);
-data2.data(inds) = {NaN};
-data2.data = cell2mat(data2.data);
+hit_data = cell2mat(hit_data);
 
 % Average data for identical ORFs that appear multiple times
-[t,t2] = grpstats(data2.data, data2.orfs, {'gname','mean'});
-kim_cunningham_2012.orfs = t;
-kim_cunningham_2012.data = t2;
-kim_cunningham_2012.ph = strcat(phenotypes, {'; '}, treatments);
+[t,t2] = grpstats(hit_data, hit_orfs, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [19; 243];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+kim_cunningham_2012.orfs = hit_orfs;
+kim_cunningham_2012.ph = hit_data_names;
+kim_cunningham_2012.data = hit_data;
+kim_cunningham_2012.dataset_ids = hit_data_ids;
+
+%% Save
 
 save('./kim_cunningham_2012.mat','kim_cunningham_2012');
+
+%% Print out
 
 fid = fopen('./kim_cunningham_2012.txt','w');
 write_matrix_file(fid, kim_cunningham_2012.orfs, kim_cunningham_2012.ph, kim_cunningham_2012.data);
