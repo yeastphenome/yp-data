e09c2bfe3075c9cf368433aacf7c132e7a8d8ed5
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-07-06 15:22:57 -0400

    Updated code & data for Blackburn & Avery, 2003

--- Datasets/12543677/code.m
+++ Datasets/12543677/code.m
@@ -6,37 +6,70 @@ addpath(genpath('../../Yeast-Matlab-Utils/'));
 FILENAMES = {};
 % TESTED = not available
 
-blackburn_avery_2003.source = {'manuscript PDF'};
-blackburn_avery_2003.downloaddate = {'2014-02-12'};
 blackburn_avery_2003.pmid = 12543677;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(blackburn_avery_2003.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%%
 
 [FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/blackburn_avery_2003_data.xlsx', 'data.txt');
 
-phenotypes = {'growth (MIC)'};
 treatments = data.raw(1,2:8)';
 data.raw(1,:) = [];
 
 % Eliminate white spaces before/after ORF
-data.raw(:,1) = clean_orf(data.raw(:,1));
+hit_orfs = data.raw(:,1);
+hit_data = data.raw(:,2:8);
+
+hit_orfs = clean_orf(hit_orfs);
 
 % Eliminate everything that doesn't look like an ORF
-inds = find(~is_orf(data.raw(:,1)));
-data.raw(inds,:) = [];
+inds = find(~is_orf(hit_orfs));
+disp(hit_orfs(inds));
 
 % Replace 'Inf' with Inf
-t = data.raw(:,2:end);
-t(~cellfun(@isnumeric, t)) = {Inf};
+hit_data(~cellfun(@isnumeric, hit_data)) = {5120};  % MIC=Inf set to 10X the maximum MIC detected
+hit_data = cell2mat(hit_data);
 
-blackburn_avery_2003.orfs = upper(data.raw(:,1));
-blackburn_avery_2003.data = cell2mat(t);
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [391; 393; 395; 69; 392; 394; 396];
 
-blackburn_avery_2003.ph = [strcat(phenotypes{1}, '; ', treatments)];
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+blackburn_avery_2003.orfs = hit_orfs;
+blackburn_avery_2003.data = hit_data;
+blackburn_avery_2003.ph = hit_data_names;
+blackburn_avery_2003.dataset_ids = hit_data_ids;
+
+%% Save
 
 save('./blackburn_avery_2003.mat','blackburn_avery_2003');
+
+%% Print out
 
 fid = fopen('./blackburn_avery_2003.txt','w');
 write_matrix_file(fid, blackburn_avery_2003.orfs, blackburn_avery_2003.ph, blackburn_avery_2003.data);
 fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(blackburn_avery_2003)
+end
 
 end
 
