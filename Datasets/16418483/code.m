353045e4c4aba3589642e5a5b19e0aa31bb91092
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-07-12 17:47:22 -0400

    Added code & data for Power~Fields, 2006

---
+++ Datasets/16418483/code.m
@@ -0,0 +1,74 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+powers_fields_2006.pmid = 16418483;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(powers_fields_2006.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/PowersRankedDeletions.xlsx', 'Sheet1');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data(5:end,2);
+
+% Get the data itself
+hit_data = cell2mat(data(5:end,5:8));
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_orf(hit_strains);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
+
+% If the same strain is present more than once, average its values
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [1306; 4778; 4779; 4780];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+powers_fields_2006.orfs = hit_strains;
+powers_fields_2006.ph = hit_data_names;
+powers_fields_2006.data = hit_data;
+powers_fields_2006.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./powers_fields_2006.mat','powers_fields_2006');
+
+%% Print out
+
+fid = fopen('./powers_fields_2006.txt','w');
+write_matrix_file(fid, powers_fields_2006.orfs, powers_fields_2006.ph, powers_fields_2006.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(powers_fields_2006)
+end
+
+end
+
