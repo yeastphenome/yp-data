8ffb245e1a61da9de9136ab0561532e2fea0b2ad
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-10-20 15:35:52 -0400

    Added raw data and code for PMID 15645503

---
+++ Datasets/15645503/code.m
@@ -0,0 +1,80 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+kang_jiang_2005.pmid = 15645503;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(kang_jiang_2005.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('readtable','./raw_data/FilamentationTable3.txt', 'delimiter','\t','HeaderLines',2);
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data.ORF;
+
+% Get the data itself
+hit_data = -ones(size(hit_strains));
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_orf(hit_strains);
+
+% If possible, fix the problem (typos, omissions etc.)
+hit_strains(ismember(hit_strains, {'YBR1401'})) = {'YBR191W'};  % weird typo, fixed based on the gene name (RPL21A)
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
+
+hit_strains(inds) = [];
+hit_data(inds,:) = [];
+
+% If the same strain is present more than once, average its values
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [574];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+kang_jiang_2005.orfs = hit_strains;
+kang_jiang_2005.ph = hit_data_names;
+kang_jiang_2005.data = hit_data;
+kang_jiang_2005.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./kang_jiang_2005.mat','kang_jiang_2005');
+
+%% Print out
+
+fid = fopen('./kang_jiang_2005.txt','w');
+write_matrix_file(fid, kang_jiang_2005.orfs, kang_jiang_2005.ph, kang_jiang_2005.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(kang_jiang_2005)
+end
+
+end
+
