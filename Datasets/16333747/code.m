c0a179e76e21ae62e067a4c87810b335e4e0bcee
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-11-01 16:46:03 -0400

    Added code and raw data for PMID 16333747

---
+++ Datasets/16333747/code.m
@@ -0,0 +1,77 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+kakimoto_yoshimura_2005.pmid = 16333747;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(kakimoto_yoshimura_2005.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('textscan','./raw_data/hits.txt', '%s');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data(:,1);
+
+% Get the data itself
+hit_data = -ones(size(hit_strains));
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_genename(hit_strains);
+
+% If in gene name form, transform into ORF name
+hit_strains = translate(hit_strains);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
+
+% If the same strain is present more than once, average its values
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [118];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+kakimoto_yoshimura_2005.orfs = hit_strains;
+kakimoto_yoshimura_2005.ph = hit_data_names;
+kakimoto_yoshimura_2005.data = hit_data;
+kakimoto_yoshimura_2005.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./kakimoto_yoshimura_2005.mat','kakimoto_yoshimura_2005');
+
+%% Print out
+
+fid = fopen('./kakimoto_yoshimura_2005.txt','w');
+write_matrix_file(fid, kakimoto_yoshimura_2005.orfs, kakimoto_yoshimura_2005.ph, kakimoto_yoshimura_2005.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(kakimoto_yoshimura_2005)
+end
+
+end
+
