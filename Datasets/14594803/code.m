4253cf5b9850996fd149bbe2f510936a73f67df7
Anastasia Baryshnikova <abarysh@princeton.edu>
2016-11-21 16:36:59 -0500

    Updated code & data

--- Datasets/14594803/code.m
+++ Datasets/14594803/code.m
@@ -16,48 +16,56 @@ datasets.standard_name = d{2};
 
 %% Load the data
 
-[~,sheets] = xlsfinfo('./raw_data/Copy of Kaplan Experimental diploid homozygous deletion srceen Release 1 and 2 sort sheet(3).xlsx');
-[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/Copy of Kaplan Experimental diploid homozygous deletion srceen Release 1 and 2 sort sheet(3).xlsx', sheets{4});
+filename = './raw_data/Copy of Kaplan Experimental diploid homozygous deletion srceen Release 1 and 2 sort sheet(3).xlsx';
+[~,sheets] = xlsfinfo(filename);
+[FILENAMES{end+1}, data] = read_data('xlsread',filename, sheets{4});
 
 % Get the list of ORFs and the correponding data 
 hit_strains = data(3:end,3);
 
 % Get the data itself
-hit_data = data(3:end, 7:46); 
+hit_data = data(3:end, 7:end); 
+hit_screens = data(1,7:end)';
+
+inds = find(cellfun(@isnumeric, hit_screens));
+hit_screens(inds) = [];
+hit_data(:,inds) = [];
+
+hit_screens = strtrim(hit_screens);
 
 % Eliminate all white spaces & capitalize
 hit_strains = clean_orf(hit_strains); 
 
 % If possible, fix the problem (typos, omissions etc.)
 hit_strains(ismember(hit_strains, {'TAL004W'})) = {'YAL004W'};
-hit_strains(ismember(hit_strains, {'YMR41W'})) = {'YMR041W'};
 hit_strains(ismember(hit_strains, {'YELOO1C'})) = {'YEL001C'};
-hit_strains(ismember(hit_strains, {'YCR102W--'})) = {'YCR102W'};
-hit_strains(ismember(hit_strains, {'YDL133C--'})) = {'YDL133C'};
-hit_strains(ismember(hit_strains, {'YKL096W--'})) = {'YKL096W'};
-hit_strains(ismember(hit_strains, {'YAR002C--'})) = {'YAR002C'};
-hit_strains(ismember(hit_strains, {'YBR084C--'})) = {'YBR084C'};
-hit_strains(ismember(hit_strains, {'YCL026C--'})) = {'YCL026C'};
-hit_strains(ismember(hit_strains, {'YCR028C--'})) = {'YCR028C'};
-hit_strains(ismember(hit_strains, {'YOR298C--'})) = {'YOR298C'};
-hit_strains(ismember(hit_strains, {'YPL183W--'})) = {'YPL183W'};
-hit_strains(ismember(hit_strains, {'YHR132W--'})) = {'YHR132W'};
-hit_strains(ismember(hit_strains, {'YKL053C--'})) = {'YKL053C'};
-hit_strains(ismember(hit_strains, {'YOR298C--'})) = {'YOR298C'};
-hit_strains(ismember(hit_strains, {'YFL013W--'})) = {'YFL013W'};
-hit_strains(ismember(hit_strains, {'YLR390W--'})) = {'YLR390W'};
-hit_strains(ismember(hit_strains, {'YFL013W--'})) = {'YFL013W'};
-hit_strains(ismember(hit_strains, {'YLR390W--'})) = {'YLR390W'};
-hit_strains(ismember(hit_strains, {'YBR090C--'})) = {'YBR090C'};
-hit_strains(ismember(hit_strains, {'YBR162W--'})) = {'YBR162W'};
-hit_strains(ismember(hit_strains, {'YER119C--'})) = {'YER119C'};
-hit_strains(ismember(hit_strains, {'YFL010W--'})) = {'YFL010W'};
-hit_strains(ismember(hit_strains, {'YPL183W--'})) = {'YPL183W'};
-hit_strains(ismember(hit_strains, {'YGR122C--'})) = {'YGR122C'};
-hit_strains(ismember(hit_strains, {'YPR133W--'})) = {'YPR133W'};
-hit_strains(ismember(hit_strains, {'YIL015C--'})) = {'YIL015C'};
-hit_strains(ismember(hit_strains, {'YDL045W--'})) = {'YDL045W'};
-hit_strains(ismember(hit_strains, {'YER014C--'})) = {'YER014C'};
+hit_strains(ismember(hit_strains, {'YCR102W-'})) = {'YCR102W'};
+hit_strains(ismember(hit_strains, {'YDL133C-'})) = {'YDL133C'};
+hit_strains(ismember(hit_strains, {'YKL096W-'})) = {'YKL096W'};
+hit_strains(ismember(hit_strains, {'YAR002C-'})) = {'YAR002C'};
+hit_strains(ismember(hit_strains, {'YBR084C-'})) = {'YBR084C'};
+hit_strains(ismember(hit_strains, {'YCL026C-'})) = {'YCL026C'};
+hit_strains(ismember(hit_strains, {'YCR028C-'})) = {'YCR028C'};
+hit_strains(ismember(hit_strains, {'YOR298C-'})) = {'YOR298C'};
+hit_strains(ismember(hit_strains, {'YPL183W-'})) = {'YPL183W'};
+hit_strains(ismember(hit_strains, {'YHR132W-'})) = {'YHR132W'};
+hit_strains(ismember(hit_strains, {'YKL053C-'})) = {'YKL053C'};
+hit_strains(ismember(hit_strains, {'YOR298C-'})) = {'YOR298C'};
+hit_strains(ismember(hit_strains, {'YFL013W-'})) = {'YFL013W'};
+hit_strains(ismember(hit_strains, {'YLR390W-'})) = {'YLR390W'};
+hit_strains(ismember(hit_strains, {'YFL013W-'})) = {'YFL013W'};
+hit_strains(ismember(hit_strains, {'YLR390W-'})) = {'YLR390W'};
+hit_strains(ismember(hit_strains, {'YBR090C-'})) = {'YBR090C'};
+hit_strains(ismember(hit_strains, {'YBR162W-'})) = {'YBR162W'};
+hit_strains(ismember(hit_strains, {'YER119C-'})) = {'YER119C'};
+hit_strains(ismember(hit_strains, {'YFL010W-'})) = {'YFL010W'};
+hit_strains(ismember(hit_strains, {'YPL183W-'})) = {'YPL183W'};
+hit_strains(ismember(hit_strains, {'YGR122C-'})) = {'YGR122C'};
+hit_strains(ismember(hit_strains, {'YPR133W-'})) = {'YPR133W'};
+hit_strains(ismember(hit_strains, {'YIL015C-'})) = {'YIL015C'};
+hit_strains(ismember(hit_strains, {'YDL045W-'})) = {'YDL045W'};
+hit_strains(ismember(hit_strains, {'YER014C-'})) = {'YER014C'};
+hit_strains(ismember(hit_strains, {'KL187C'})) = {'YKL187C'};
 
 % Find anything that doesn't look like an ORF
 inds = find(~is_orf(hit_strains));
@@ -71,13 +79,18 @@ hit_data = cell2mat(hit_data);
 % If the same strain is present more than once, average its values
 [hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
 
-hit_data(:, 32:35) = [];
-hit_data(:, 9:12) = [];
-
 % MANUAL. Get the dataset ids corresponding to each dataset (in order)
 % Multiple datasets (e.g., replicates) may get the same id, which can then
 % be used to average them out
-hit_data_ids = [483; 5185; 5186; 5187; 5188; 5189; 5190; 5191; 5193; 5194; 5195; 5196; 5197; 5198; 5199; 5200; 5201;5202;5203;5204;5205;5206;5207;5208;5209;5210;5211;5212;5213;5214;5215;5216;];
+
+[FILENAMES{end+1}, dt] = read_data('textscan','./extras/dataset_ids.txt','%s %d','delimiter','\t');
+[Lia, Locb] = ismember(hit_screens, strtrim(dt{1}));
+
+hit_data_ids = dt{2}(Locb);
+
+[hit_data_ids, hit_data] = grpstats(hit_data', hit_data_ids, {'gname','mean'});
+hit_data = hit_data';
+hit_data_ids = cellfun(@str2num, hit_data_ids);
 
 %% Prepare final dataset
 
