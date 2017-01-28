4cfe544003e3836591e534220c92838744f61ab9
Brianna Richardson <bricha2@umbc.edu>
2017-01-27 21:57:11 -0500

    newCode()

---
+++ Datasets/18160327/code.m
@@ -0,0 +1,107 @@
+%% Dilda~Hogg, 2008
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+dilda_hogg_2008.pmid = 18160327;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(dilda_hogg_2008.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, sensitive_data] = read_data('xlsread','./raw_data/mmc.xlsx', 'Table1');
+
+% Get the list of ORFs and translate
+inds = find(is_genename(sensitive_data));
+sensitive_strains = sensitive_data(inds);
+sensitive_strains = translate(sensitive_strains);
+
+% Get the data
+sensitive_data = sensitive_data(inds+1);
+sensitive_data = cell2mat(sensitive_data);
+
+% If possible, fix the problem (typos, omissions etc.)
+sensitive_strains(ismember(sensitive_strains, {'CRS5'})) = {'YOR031W'};
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(sensitive_strains));
+sensitive_strains(inds) = [];
+sensitive_data(inds) = [];
+
+% If the same strain is present more than once, average its values
+[sensitive_strains, sensitive_data] = grpstats(sensitive_data, sensitive_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [1323];
+
+%% Now retrieve resistant tables
+[FILENAMES{end+1}, resistant_data] = read_data('xlsread','./raw_data/mmc.xlsx', 'Table2');
+
+% Get the list of ORFs and translate
+inds = find(is_genename(resistant_data));
+resistant_strains = resistant_data(inds);
+resistant_strains = translate(resistant_strains);
+
+% Get the data
+resistant_data = resistant_data(inds+1);
+ind = find(~cellfun(@isnumeric, resistant_data));
+resistant_data{ind} = [4000];
+resistant_data = cell2mat(resistant_data);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(resistant_strains));
+resistant_strains(inds) = [];
+resistant_data(inds) = [];
+
+% If the same strain is present more than once, average its values
+[resistant_strains, resistant_data] = grpstats(resistant_data, resistant_strains, {'gname','mean'});
+
+%% Combine data
+
+hit_strains = [resistant_strains; sensitive_strains];
+hit_data = [resistant_data; sensitive_data];
+
+% Transform data
+hit_data = log(hit_data / 1100);
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+dilda_hogg_2008.orfs = hit_strains;
+dilda_hogg_2008.ph = hit_data_names;
+dilda_hogg_2008.data = hit_data;
+dilda_hogg_2008.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./dilda_hogg_2008.mat','dilda_hogg_2008');
+
+%% Print out
+
+fid = fopen('./dilda_hogg_2008.txt','w');
+write_matrix_file(fid, dilda_hogg_2008.orfs, dilda_hogg_2008.ph, dilda_hogg_2008.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(dilda_hogg_2008)
+end
+
+end
+
