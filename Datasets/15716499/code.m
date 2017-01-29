95eb1187017fcb7ec87497a669c01ce8ee60b74a
Anastasia Baryshnikova <abarysh@princeton.edu>
2017-01-29 11:20:01 -0800

    Fixed dataset name, compressed code

--- Datasets/15716499/code.m
+++ Datasets/15716499/code.m
@@ -4,13 +4,13 @@ function FILENAMES = code()
 addpath(genpath('../../Yeast-Matlab-Utils/'));
 
 FILENAMES = {};
-deutschbauer_giaevere_2005.pmid = 15716499;
+deutschbauer_giaever_2005.pmid = 15716499;
 
 % MANUAL. Download the list of dataset ids and standard names from
 % the paper's page on www.yeastphenome.org & save the file to ./extras
 
 % Load the list
-[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(deutschbauer_giaevere_2005.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(deutschbauer_giaever_2005.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
 datasets.id = d{1};
 datasets.standard_name = d{2};
 
@@ -23,11 +23,7 @@ hit_strains = data(2:end,1);
 
 % Get the data itself
 hit_data = data(2:end,[9 13 15 17]);
-for i = 1:4
-    temp = hit_data(:,i);
-    temp(find(~cellfun(@isnumeric, temp))) = {NaN};
-    hit_data(:,i) = temp;
-end
+hit_data(find(~cellfun(@isnumeric, hit_data))) = {NaN};
 hit_data = cell2mat(hit_data);
 
 % Eliminate all white spaces & capitalize
@@ -54,26 +50,26 @@ hit_data_names = cell(size(hit_data_ids));
 hit_data_names(ind2) = datasets.standard_name(ind1);
 
 % If the dataset is quantitative:
-deutschbauer_giaevere_2005.orfs = hit_strains;
-deutschbauer_giaevere_2005.ph = hit_data_names;
-deutschbauer_giaevere_2005.data = hit_data;
-deutschbauer_giaevere_2005.dataset_ids = hit_data_ids;
+deutschbauer_giaever_2005.orfs = hit_strains;
+deutschbauer_giaever_2005.ph = hit_data_names;
+deutschbauer_giaever_2005.data = hit_data;
+deutschbauer_giaever_2005.dataset_ids = hit_data_ids;
 
 %% Save
 
-save('./deutschbauer_giaevere_2005.mat','deutschbauer_giaevere_2005');
+save('./deutschbauer_giaever_2005.mat','deutschbauer_giaever_2005');
 
 %% Print out
 
-fid = fopen('./deutschbauer_giaevere_2005.txt','w');
-write_matrix_file(fid, deutschbauer_giaevere_2005.orfs, deutschbauer_giaevere_2005.ph, deutschbauer_giaevere_2005.data);
+fid = fopen('./deutschbauer_giaever_2005.txt','w');
+write_matrix_file(fid, deutschbauer_giaever_2005.orfs, deutschbauer_giaever_2005.ph, deutschbauer_giaever_2005.data);
 fclose(fid);
 
 %% Save to DB (admin)
 
 addpath(genpath('../../Private-Utils/'));
 if exist('save_data_to_db.m')
-    res = save_data_to_db(deutschbauer_giaevere_2005)
+    res = save_data_to_db(deutschbauer_giaever_2005)
 end
 
 end
