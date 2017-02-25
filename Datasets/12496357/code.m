866b9c87f5b91b84e480de216d790b50ef0c4072
Anastasia Baryshnikova <abarysh@princeton.edu>
2017-02-24 19:09:11 -0800

    Standardized code

--- Datasets/12496357/code.m
+++ Datasets/12496357/code.m
@@ -4,9 +4,19 @@ function FILENAMES = code()
 addpath(genpath('../../Yeast-Matlab-Utils/'));
 
 FILENAMES = {};
+begley_samson_2002.pmid = 12496357;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(begley_samson_2002.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
 % NOTE = data sorting fixed (lower=slower)
 
-begley_samson_2002.pmid = 12496357;
 
 phenotype = {'Growth, dilution spot intensity'};
 
@@ -82,40 +92,68 @@ data2.data_avg(:,4) = nanmean(data2.data(:,4:4:end),2);
 
 for ic = 1 : length(treatments)
 
-inds_cond = strmatch(treatments{ic}, data2.conditions);
+    inds_cond = strmatch(treatments{ic}, data2.conditions);
 
-data3(ic).orfs = data2.orfs(inds_cond);
-data3(ic).data = data2.data_avg(inds_cond,:);
+    data3(ic).orfs = data2.orfs(inds_cond);
+    data3(ic).data = data2.data_avg(inds_cond,:);
 
-% Average data for identical ORFs that appear multiple times in each
-% dataset
-[t,t2] = grpstats(data3(ic).data, data3(ic).orfs, {'gname','mean'});
-data4(ic).orfs = t;
-data4(ic).data = t2;
+    % Average data for identical ORFs that appear multiple times in each
+    % dataset
+    [t,t2] = grpstats(data3(ic).data, data3(ic).orfs, {'gname','mean'});
+    data4(ic).orfs = t;
+    data4(ic).data = t2;
 
-% Make sure the final list of all ORFs is comprehensive
-if ic == 1
-begley_samson_2002.orfs = data4(ic).orfs;
-begley_samson_2002.data = zeros(length(data4(ic).orfs),length(doses(:)))+NaN;
-begley_samson_2002.ph = cell(length(doses(:)),1);
+    % Make sure the final list of all ORFs is comprehensive
+    if ic == 1
+        begley_samson_2002.orfs = data4(ic).orfs;
+        begley_samson_2002.data = zeros(length(data4(ic).orfs),length(doses(:)))+NaN;
+        begley_samson_2002.ph = cell(length(doses(:)),1);
+    end
+    inds = find(~ismember(data4(ic).orfs, begley_samson_2002.orfs));
+    begley_samson_2002.orfs = [begley_samson_2002.orfs; data4(ic).orfs(inds)];
+
+    % Now it is possible to intersect this dataset with the comprehensive
+    % list, align and append all the values
+    [C, ia,ib] = intersect(begley_samson_2002.orfs, data4(ic).orfs);
+    begley_samson_2002.data(ia,4*(ic-1)+1:4*(ic-1)+4) = data4(ic).data(ib,:);
+    begley_samson_2002.ph(4*(ic-1)+1:4*(ic-1)+4) = strcat(phenotype, {'; '}, treatments(ic),{', '}, doses(ic,:))';
+
 end
-inds = find(~ismember(data4(ic).orfs, begley_samson_2002.orfs));
-begley_samson_2002.orfs = [begley_samson_2002.orfs; data4(ic).orfs(inds)];
 
-% Now it is possible to intersect this dataset with the comprehensive
-% list, align and append all the values
-[C, ia,ib] = intersect(begley_samson_2002.orfs, data4(ic).orfs);
-begley_samson_2002.data(ia,4*(ic-1)+1:4*(ic-1)+4) = data4(ic).data(ib,:);
-begley_samson_2002.ph(4*(ic-1)+1:4*(ic-1)+4) = strcat(phenotype, {'; '}, treatments(ic),{', '}, doses(ic,:))';
+%%
 
+[FILENAMES{end+1}, phenotype_dataset] = read_data('textread','./extras/phenotype_datasetids.txt', '%s %d','delimiter','\t');
+hit_data_ids = nan(length(begley_samson_2002.ph),1);
+[~,ind1,ind2] = intersect(begley_samson_2002.ph, phenotype_dataset{1});
+hit_data_ids(ind1) = phenotype_dataset{2}(ind2);
 
-end
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+begley_samson_2002.ph = hit_data_names;
+begley_samson_2002.dataset_ids = hit_data_ids;
+
+%% Save
 
 save('./begley_samson_2002.mat','begley_samson_2002');
+
+%% Print out
 
 fid = fopen('./begley_samson_2002.txt','w');
 write_matrix_file(fid, begley_samson_2002.orfs, begley_samson_2002.ph, begley_samson_2002.data);
 fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(begley_samson_2002)
+end
 
 end
 
