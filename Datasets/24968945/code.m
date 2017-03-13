af9b24ad78eeafe62b541e9216811a79ac520fdd
Brianna Richardson <bricha2@umbc.edu>
2017-03-13 12:52:14 -0400

    Updated Code

--- Datasets/24968945/code.m
+++ Datasets/24968945/code.m
@@ -6,10 +6,14 @@ addpath(genpath('../../Yeast-Matlab-Utils/'));
 FILENAMES = {};
 gaupel_tenniswood_2004.pmid = 24968945;
 
-phenotypes = {'growth'};
-treatments = {'CG-1521'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(gaupel_tenniswood_2004.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
 
-% Load data (1)
+%% Load the data
 [FILENAMES{end+1}, data_sens.raw] = read_data('xlsread','./raw_data/1471-2164-15-528-s1.xlsx', 'Top sensitive strains');
 
 hits_orfs1 = data_sens.raw(2:end,1);
@@ -50,32 +54,62 @@ hits_orfs2(ind2) = []; hits_data2(ind2,:) = [];
 hits_orfs = [hits_orfs1; hits_orfs2];
 hits_data = [hits_data1; hits_data2];
 
-% Load tested
+%% Tested strains
+
+% Load tested strains
 [FILENAMES{end+1}, tested.raw] = read_data('xlsread','./raw_data/CompleteDeletionLibrary.xlsx');
 tested_orfs = tested.raw(2:end,1);
 
+% Remove all indices without a name
 inds = find(cellfun(@isempty, tested_orfs) | cellfun(@isnumeric, tested_orfs));
 tested_orfs(inds) = [];
 
+% Take the unique set
 tested_orfs = unique(strtrim(upper(tested_orfs)));
 
+% Remove non-relevant indices
 inds = find(~strncmp('Y', tested_orfs,1));
 tested_orfs(inds) = [];
 
+% Make sure that all the hits are part of the tested set
 [missing, ix] = setdiff(hits_orfs, tested_orfs);
 
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [569];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is discrete/binary and the tested strains were provided separately:
 gaupel_tenniswood_2004.orfs = tested_orfs;
-gaupel_tenniswood_2004.data = zeros(length(tested_orfs),1);
+gaupel_tenniswood_2004.ph = hit_data_names;
+gaupel_tenniswood_2004.data = zeros(length(gaupel_tenniswood_2004.orfs),length(gaupel_tenniswood_2004.ph));
+gaupel_tenniswood_2004.dataset_ids = hit_data_ids;
 
 [~,ind1,ind2] = intersect(hits_orfs, tested_orfs);
 gaupel_tenniswood_2004.data(ind2,:) = hits_data(ind1,:);
 
-gaupel_tenniswood_2004.ph = strcat(phenotypes, '; ', treatments);
+%% Save
 
 save('./gaupel_tenniswood_2004.mat','gaupel_tenniswood_2004');
+
+%% Print out
 
 fid = fopen('./gaupel_tenniswood_2004.txt','w');
 write_matrix_file(fid, gaupel_tenniswood_2004.orfs, gaupel_tenniswood_2004.ph, gaupel_tenniswood_2004.data);
 fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(deutschbauer_giaevere_2005)
+end
 
 end
