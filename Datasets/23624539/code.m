8228a085c4e5d628a37f6018ac8eb3036a2dd800
Brianna Richardson <bricha2@umbc.edu>
2017-03-13 13:40:17 -0400

    Updated code

--- Datasets/23624539/code.m
+++ Datasets/23624539/code.m
@@ -4,15 +4,16 @@ function FILENAMES = code()
 addpath(genpath('../../Yeast-Matlab-Utils/'));
 
 FILENAMES = {};
-
-
-galvan_marquez_smith_2013.source = {'Imelda Galvan Marquez'};
-galvan_marquez_smith_2013.downloaddate = {'2013-05-19'};
 galvan_marquez_smith_2013.pmid = 23624539;
 
-phenotypes = {'Growth, colony size'};
-treatments = {'Chitosan'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(galvan_marquez_smith_2013.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
 
+%% Load the data
 [FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/Chitosan Effect on GDA (raw data). Exp. 1 to 3. Imelda Galvan, 2013-1.xlsx', 'Sheet1');
 
 % Get indices of the data columns
@@ -38,21 +39,40 @@ data2.data = nanmean(cell2mat(t),2);
 
 % Average data for identical ORFs that appear multiple times
 [t,t2] = grpstats(data2.data, data2.orfs, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [129];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
 galvan_marquez_smith_2013.orfs = t;
+galvan_marquez_smith_2013.ph = hit_data_names;
 galvan_marquez_smith_2013.data = t2;
-galvan_marquez_smith_2013.ph = strcat(phenotypes, '; ', treatments);
+galvan_marquez_smith_2013.dataset_ids = hit_data_ids;
 
-
-% Eliminate the essential genes
-load essential_genes_100908;
-[t,ind1,ind2] = intersect(galvan_marquez_smith_2013.orfs, essential_genes);
-galvan_marquez_smith_2013.orfs(ind1) = [];
-galvan_marquez_smith_2013.data(ind1,:) = [];
+%% Save
 
 save('./galvan_marquez_smith_2013.mat','galvan_marquez_smith_2013');
+
+%% Print out
 
 fid = fopen('./galvan_marquez_smith_2013.txt','w');
 write_matrix_file(fid, galvan_marquez_smith_2013.orfs, galvan_marquez_smith_2013.ph, galvan_marquez_smith_2013.data);
 fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(galvan_marquez_smith_2013)
+end
 
 end
