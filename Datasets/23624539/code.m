d1a4277e9436b675901caa614c53fa8be233d095
Anastasia Baryshnikova <abarysh@princeton.edu>
2017-03-13 18:53:38 -0700

    Added clean_orf

--- Datasets/23624539/code.m
+++ Datasets/23624539/code.m
@@ -19,26 +19,23 @@ datasets.standard_name = d{2};
 % Get indices of the data columns
 ind_data = find(strcmp('Ratio', data.raw(1,:)));
 
-% Eliminate anything that doesn't look like an ORF
-inds = find(cellfun(@isnumeric, data.raw(:,2)));
-data.raw(inds,:) = [];
+hit_strains = data.raw(:,2);
+hit_data = data.raw(:,ind_data);
 
-inds = setdiff(1:length(data.raw(:,2)),strmatch('Y', data.raw(:,2)));
-data.raw(inds,:) = [];
+hit_strains = clean_orf(hit_strains);
 
-% Eliminate white spaces before/after ORF
-data.raw(:,3) = cellfun(@strtrim, data.raw(:,2),'UniformOutput',0);
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
 
-% Make sure all the data are numbers
-t = data.raw(:,ind_data);
-inds = find(cellfun(@isnumeric, t)==0);
-t(inds) = {NaN};
+hit_strains(inds) = [];
+hit_data(inds,:) = [];
 
-data2.orfs = upper(data.raw(:,2));
-data2.data = nanmean(cell2mat(t),2);
+hit_data = cell2mat(hit_data);
+hit_data = nanmean(hit_data,2);
 
 % Average data for identical ORFs that appear multiple times
-[t,t2] = grpstats(data2.data, data2.orfs, {'gname','mean'});
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
 
 % MANUAL. Get the dataset ids corresponding to each dataset (in order)
 % Multiple datasets (e.g., replicates) may get the same id, which can then
@@ -53,9 +50,9 @@ hit_data_names = cell(size(hit_data_ids));
 hit_data_names(ind2) = datasets.standard_name(ind1);
 
 % If the dataset is quantitative:
-galvan_marquez_smith_2013.orfs = t;
+galvan_marquez_smith_2013.orfs = hit_strains;
 galvan_marquez_smith_2013.ph = hit_data_names;
-galvan_marquez_smith_2013.data = t2;
+galvan_marquez_smith_2013.data = hit_data;
 galvan_marquez_smith_2013.dataset_ids = hit_data_ids;
 
 %% Save
