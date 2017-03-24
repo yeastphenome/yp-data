a78333b86ec8075708a9affd20ad55464e94e7df
Brianna Richardson <bricha2@umbc.edu>
2017-03-24 00:41:42 -0400

    Updated code()

--- Datasets/22586468/code.m
+++ Datasets/22586468/code.m
@@ -4,15 +4,16 @@ function FILENAMES = code()
 addpath(genpath('../../Yeast-Matlab-Utils/'));
 
 FILENAMES = {};
-
-
-peyroche_plateau_2012.source = {'http://www.plosone.org/article/fetchSingleRepresentation.action?uri=info:doi/10.1371/journal.pone.0036343.s004'};
-peyroche_plateau_2012.downloaddate = {'2013-03-08'};
 peyroche_plateau_2012.pmid = 22586468;
-peyroche_plateau_2012.desc = {'Relative fitness defect: rf = log2(wt(Se)/mut(Se) - wt/mut + 1), where wt and mut are the generation times of the WT and mutant strains with and without Na2Se.'};
 
-phenotypes = {'Growth, log2 ratio'};
-treatments = {'Sodium selenide, 1 uM 16 h'; 'Sodium selenide, 2 uM 16 h'; 'Sodium selenide, 1 uM 27 h'; 'Sodium selenide, 2 uM 27 h'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(peyroche_plateau_2012.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data 
 
 [FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/journal.pone.0036343.s004.xlsx', 'data');
 
@@ -20,16 +21,13 @@ treatments = {'Sodium selenide, 1 uM 16 h'; 'Sodium selenide, 2 uM 16 h'; 'Sodiu
 ind_data = 5:8;
 
 % Eliminate anything that doesn't look like an ORF
-inds = find(cellfun(@isnumeric, data.raw(:,1)));
-data.raw(inds,:) = [];
-
-inds = setdiff(1:length(data.raw(:,1)),strmatch('Y', data.raw(:,1)));
+inds = find(~is_orf(data.raw(:,1)));
 data.raw(inds,:) = [];
 
 % Eliminate white spaces before/after ORF
-data.raw(:,1) = cellfun(@strtrim, data.raw(:,1),'UniformOutput',0);
+data.raw(:,1) = clean_orf(data.raw(:,1));
 
-data2.orfs = upper(data.raw(:,1));
+% Retrieve the data from the data set
 data2.data = data.raw(:,ind_data);
 
 % Make sure all the data are numbers
@@ -37,16 +35,45 @@ inds = find(cellfun(@isnumeric, data2.data)==0);
 data2.data(inds) = {NaN};
 data2.data = cell2mat(data2.data);
 
+% If in gene name form, transform into ORF name
+data.raw(:,1) = translate(data.raw(:,1));
+
 % Average data for identical ORFs that appear multiple times
-[t,t2] = grpstats(data2.data, data2.orfs, {'gname','mean'});
+[t,t2] = grpstats(data2.data, data.raw(:,1), {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [57; 58; 525; 526];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
 peyroche_plateau_2012.orfs = t;
+peyroche_plateau_2012.ph = hit_data_names;
 peyroche_plateau_2012.data = t2;
-peyroche_plateau_2012.ph = strcat(phenotypes, {'; '}, treatments);
+peyroche_plateau_2012.dataset_ids = hit_data_ids;
+
+%% Save
 
 save('./peyroche_plateau_2012.mat','peyroche_plateau_2012');
+
+%% Print out
 
 fid = fopen('./peyroche_plateau_2012.txt','w');
 write_matrix_file(fid, peyroche_plateau_2012.orfs, peyroche_plateau_2012.ph, peyroche_plateau_2012.data);
 fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(peyroche_plateau_2012)
+end
 
 end
