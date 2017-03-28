055c4a2ecfd7f6f7f0987d43f5a60dacbe83b630
Brianna Richardson <bricha2@umbc.edu>
2017-03-28 14:07:14 -0400

    Updated Code()

--- Datasets/21601516/code.m
+++ Datasets/21601516/code.m
@@ -6,45 +6,103 @@ addpath(genpath('../../Yeast-Matlab-Utils/'));
 FILENAMES = {};
 suzuki_yoshida_2011.pmid = 21601516;
 
-phenotypes = {'glutathione abundance'};
-treatments = {'standard'};
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
 
-% Load data
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(suzuki_yoshida_2011.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load data
 [FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/hits_data.xlsx');
 
-hits_genes = data.raw(1:end,1);
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hits_orfs = data.raw(1:end,1);
+
+% Get the data itself
 hits_data = cell2mat(data.raw(1:end,2));
 
-hits_genes = clean_genename(hits_genes);
-hits_orfs = translate(hits_genes);
+% Eliminate all white spaces & capitalize
+hits_orfs = clean_genename(hits_orfs);
 
-[t,t2] = grpstats(hits_data,hits_orfs,{'gname','mean'});
-hits_orfs = t;
-hits_data = t2;
+% If in gene name form, transform into ORF name
+hits_orfs = translate(hits_orfs);
 
-% Load tested
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hits_orfs));
+hits_orfs(inds) = [];
+hits_data(inds) = [];
+
+% If the same strain is present more than once, average its values
+[hits_orfs,hit_data] = grpstats(hits_data,hits_orfs,{'gname','mean'});
+
+%% Load tested
 [FILENAMES{end+1}, tested.raw] = read_data('xlsread','./raw_data/YKOmatalpha_GSH_list070508.xlsx');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
 tested_orfs = tested.raw(3:end,3);
 
+% Eliminate all white spaces & capitalize
+tested_orfs = clean_orf(tested_orfs);
+
+% Find empty matrices and remove them
 inds = find(cellfun(@isempty, tested_orfs) | cellfun(@isnumeric, tested_orfs));
 tested_orfs(inds) = [];
 
-tested_orfs = unique(upper(clean_orf(tested_orfs)));
+% If in gene name form, transform into ORF name
+tested_orfs = translate(tested_orfs);
 
+% Find anything that doesn't look like an ORF
+tested_orfs(ismember(tested_orfs, {'YAL043C-a'})) = {'YAL042C-A'};
+tested_orfs(ismember(tested_orfs, {'YML009c'})) = {'YML009C'};
+inds = find(~is_orf(tested_orfs));
+tested_orfs(inds) = [];
+
+% Get a unique list of ORFs
+tested_orfs = unique(tested_orfs);
+
+% Isolate difference between tested and orfs
 [missing, ix] = setdiff(hits_orfs, tested_orfs);
 
-suzuki_yoshida_2011.orfs = tested_orfs;
-suzuki_yoshida_2011.data = zeros(length(tested_orfs),1);
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [125];
 
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+suzuki_yoshida_2011.orfs = tested_orfs;
+suzuki_yoshida_2011.ph = hit_data_names;
+suzuki_yoshida_2011.data = zeros(length(tested_orfs),1);
 [~,ind1,ind2] = intersect(hits_orfs, tested_orfs);
 suzuki_yoshida_2011.data(ind2,:) = hits_data(ind1,:);
+suzuki_yoshida_2011.dataset_ids = hit_data_ids;
 
-suzuki_yoshida_2011.ph = strcat(phenotypes, '; ', treatments);
+%% Save
 
 save('./suzuki_yoshida_2011.mat','suzuki_yoshida_2011');
+
+%% Print out
 
 fid = fopen('./suzuki_yoshida_2011.txt','w');
 write_matrix_file(fid, suzuki_yoshida_2011.orfs, suzuki_yoshida_2011.ph, suzuki_yoshida_2011.data);
 fclose(fid);
+
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(suzuki_yoshida_2011)
+end
 
 end
