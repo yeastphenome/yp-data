026be2419d4bbbf2aabc0c4b98f77d5bd95e30ea
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2017-04-30 21:53:26 -0700

    New code & data

---
+++ Datasets/23358190/code.m
@@ -0,0 +1,86 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+gaytan_vulpe_2013.pmid = 23358190;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(gaytan_vulpe_2013.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/toxsci_12_0972_File010.xlsx', 'mutants with altered growth in ');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data(:,1);
+
+% Get the data itself
+hit_data = data(:,2:4);
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_orf(hit_strains);
+
+inds = find(cellfun(@isnumeric, hit_strains));
+hit_strains(inds) = [];
+hit_data(inds,:) = [];
+
+% If in gene name form, transform into ORF name
+hit_strains = translate(hit_strains);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
+
+hit_strains(inds) = [];
+hit_data(inds,:) = [];
+
+hit_data = cell2mat(hit_data);
+
+% If the same strain is present more than once, average its values
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [208 207 206]';
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+gaytan_vulpe_2013.orfs = hit_strains;
+gaytan_vulpe_2013.ph = hit_data_names;
+gaytan_vulpe_2013.data = hit_data;
+gaytan_vulpe_2013.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./gaytan_vulpe_2013.mat','gaytan_vulpe_2013');
+
+%% Print out
+
+fid = fopen('./gaytan_vulpe_2013.txt','w');
+write_matrix_file(fid, gaytan_vulpe_2013.orfs, gaytan_vulpe_2013.ph, gaytan_vulpe_2013.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(gaytan_vulpe_2013)
+end
+
+end
+
