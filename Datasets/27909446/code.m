0b951009a500194e6a16367e61e426b18ada39bb
Brianna Richardson <bricha2@umbc.edu>
2017-05-08 20:15:03 -0400

    New Code()
    The sheet with the list of datasets needs to be updated. I wasn't able
    to update that file because I kept getting an error when I pressed the
    download datasets link.

---
+++ Datasets/27909446/code.m
@@ -0,0 +1,74 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+north_vulpe_2016.pmid = 27909446;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(north_vulpe_2016.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/Table1.xlsx');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data(12:end,1);
+
+% Get the data itself
+hit_data = cell2mat(data(12:end,3:8));
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_orf(hit_strains);
+
+% If in gene name form, transform into ORF name
+[hit_strains, translated, ambiguous] = translate(hit_strains);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+hit_strains(inds) = [];
+hit_data(inds, :) = [];
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [5377; 5376; 5375; 5380; 5379; 5378];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+north_vulpe_2016.orfs = hit_strains;
+north_vulpe_2016.ph = hit_data_names;
+north_vulpe_2016.data = hit_data;
+north_vulpe_2016.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./north_vulpe_2016.mat','north_vulpe_2016');
+
+%% Print out
+
+fid = fopen('./north_vulpe_2016.txt','w');
+write_matrix_file(fid, north_vulpe_2016.orfs, north_vulpe_2016.ph, north_vulpe_2016.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(north_vulpe_2016)
+end
+
+end
