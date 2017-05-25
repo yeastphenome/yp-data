2cde0ba99924d845c0c73c7f7658e8369de13b78
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2017-05-25 12:09:36 -0700

    New code & data

---
+++ Datasets/22140548/code.m
@@ -0,0 +1,89 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+franzosa_mcclellan_2011.pmid = 22140548;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(franzosa_mcclellan_2011.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/journal.pone.0028211.s003.xlsx', 'Sheet1');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data(4:end,1);
+
+% Get the data itself
+hit_data = data(4:end,2);
+
+inds = find(cellfun(@isnumeric, hit_strains));
+hit_strains(inds) = [];
+hit_data(inds) = [];
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_genename(hit_strains);
+
+hit_strains(strcmp('MRF1''', hit_strains)) = {'MRF1'};
+hit_strains(strcmp('MSF1''', hit_strains)) = {'MSF1'};
+hit_strains(strcmp('AAP1''', hit_strains)) = {'AAP1'};
+
+% If in gene name form, transform into ORF name
+hit_strains = translate(hit_strains);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
+hit_strains(inds) = [];
+hit_data(inds) = [];
+
+hit_data = cell2mat(hit_data);
+
+% If the same strain is present more than once, average its values
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [11784];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+franzosa_mcclellan_2011.orfs = hit_strains;
+franzosa_mcclellan_2011.ph = hit_data_names;
+franzosa_mcclellan_2011.data = hit_data;
+franzosa_mcclellan_2011.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./franzosa_mcclellan_2011.mat','franzosa_mcclellan_2011');
+
+%% Print out
+
+fid = fopen('./franzosa_mcclellan_2011.txt','w');
+write_matrix_file(fid, franzosa_mcclellan_2011.orfs, franzosa_mcclellan_2011.ph, franzosa_mcclellan_2011.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(franzosa_mcclellan_2011)
+end
+
+end
+
