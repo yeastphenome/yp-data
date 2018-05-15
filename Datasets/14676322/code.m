abfc0aa36c719df0b8af6a3429ca520cba66cce1
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2018-05-14 20:47:10 -0700

    Added the reference data (standard conditions)

--- Datasets/14676322/code.m
+++ Datasets/14676322/code.m
@@ -17,33 +17,70 @@ datasets.standard_name = d{2};
 
 %%
 
-phenotypes = {'Growth, lag phase';'Growth, exponential growth rate';'Growth, saturation level'};
-treatments = {'NaCl, 0.85 M'};
+[FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/LPI NaCl.xlsx', 'LPI');
 
-[FILENAMES{end+1}, data.raw] = read_data('xlsread','./raw_data/ORIG130305_LPI NaCl.xlsx', 'LPI');
-
-hit_strains = data.raw(5:end,1);
-hit_data = cell2mat(data.raw(5:end,2:4));
+hit_strains1 = data.raw(5:end,1);
+hit_data1 = cell2mat(data.raw(5:end,2:4));
 
 % Eliminate all white spaces & capitalize
-hit_strains = clean_orf(hit_strains);
+hit_strains1 = clean_orf(hit_strains1);
 
 % Find anything that doesn't look like an ORF
-inds = find(~is_orf(hit_strains));
-disp(hit_strains(inds));  
+inds = find(~is_orf(hit_strains1));
+disp(hit_strains1(inds));  
 
-hit_strains(inds) = [];
-hit_data(inds,:) = [];
+hit_strains1(inds) = [];
+hit_data1(inds,:) = [];
 
 % Average data for identical ORFs that appear multiple times
-[t,t2] = grpstats(hit_data, hit_strains, {'gname','mean'});
+[hit_strains1,hit_data1] = grpstats(hit_data1, hit_strains1, {'gname','mean'});
 
 % MANUAL. Get the dataset ids corresponding to each dataset (in order)
 % Multiple datasets (e.g., replicates) may get the same id, which can then
 % be used to average them out
-hit_data_ids = [50 49 51]';
+hit_data_ids1 = [50 49 51]';
+
+%% Load control data
+
+[FILENAMES{end+1}, data2.raw] = read_data('xlsread','./raw_data/LSC Reference.xlsx', 'LSC');
+
+hit_strains2 = data2.raw(5:end,1);
+hit_data2 = data2.raw(5:end,[4,8,12]);
+
+% Eliminate all white spaces & capitalize
+hit_strains2 = clean_orf(hit_strains2);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains2));
+disp(hit_strains2(inds));  
+
+hit_strains2(inds) = [];
+hit_data2(inds,:) = [];
+
+inds = find(~cellfun(@isnumeric, hit_data2));
+hit_data2(inds) = {NaN};
+
+hit_data2 = cell2mat(hit_data2);
+
+% Average data for identical ORFs that appear multiple times
+[hit_strains2,hit_data2] = grpstats(hit_data2, hit_strains2, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids2 = [16184 16183 16185]';
+
 
 %% Prepare final dataset
+
+hit_data_ids = [hit_data_ids1; hit_data_ids2];
+hit_strains = unique([hit_strains1; hit_strains2]);
+hit_data = nan(length(hit_strains), length(hit_data_ids));
+
+[~,ind1,ind2] = intersect(hit_strains, hit_strains1);
+hit_data(ind1,1:3) = hit_data1(ind2,:);
+[~,ind1,ind2] = intersect(hit_strains, hit_strains2);
+hit_data(ind1,4:6) = hit_data2(ind2,:);
 
 % Match the dataset ids with the dataset standard names
 [~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
