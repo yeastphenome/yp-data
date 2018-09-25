f372d8aea4f42eb9cb7a1f0552c22705dacd426c
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2018-09-24 17:00:36 -0700

    New code & data

---
+++ Datasets/15883361/code.m
@@ -0,0 +1,87 @@
+%% FirstAuthor~LastAuthor, YYYY
+function FILENAMES = code()
+
+addpath(genpath('../../Yeast-Matlab-Utils/'));
+
+FILENAMES = {};
+panavas_nagy_2005.pmid = 15883361;
+
+% MANUAL. Download the list of dataset ids and standard names from
+% the paper's page on www.yeastphenome.org & save the file to ./extras
+
+% Load the list
+[FILENAMES{end+1}, d] = read_data('textread', ['./extras/YeastPhenome_' num2str(panavas_nagy_2005.pmid) '_datasets_list.txt'],'%d %s','delimiter','\t');
+datasets.id = d{1};
+datasets.standard_name = d{2};
+
+%% Load the data
+
+[FILENAMES{end+1}, data] = read_data('xlsread','./raw_data/hits.xlsx', 'Sheet1');
+
+% Get the list of ORFs and the correponding data 
+% (this part usually changes significantly based on the format of the raw data file)
+hit_strains = data(:,1);
+
+% Get the data itself
+hit_data = data(:,2);
+
+inds = find(cellfun(@isnumeric, hit_strains));
+hit_strains(inds) = [];
+hit_data(inds) = [];
+   
+% Eliminate all white spaces & capitalize
+hit_strains = clean_genename(hit_strains);
+
+% If in gene name form, transform into ORF name
+hit_strains = translate(hit_strains);
+
+% Find anything that doesn't look like an ORF
+inds = find(~is_orf(hit_strains));
+disp(hit_strains(inds));  
+
+hit_strains(inds) = [];
+hit_data(inds) = [];
+
+hit_data = regexprep(hit_data, '[^0-9]','');
+hit_data = cellfun(@str2num, hit_data);
+
+% If the same strain is present more than once, average its values
+[hit_strains, hit_data] = grpstats(hit_data, hit_strains, {'gname','mean'});
+
+% MANUAL. Get the dataset ids corresponding to each dataset (in order)
+% Multiple datasets (e.g., replicates) may get the same id, which can then
+% be used to average them out
+hit_data_ids = [16007];
+
+%% Prepare final dataset
+
+% Match the dataset ids with the dataset standard names
+[~,ind1,ind2] = intersect(datasets.id, hit_data_ids);
+hit_data_names = cell(size(hit_data_ids));
+hit_data_names(ind2) = datasets.standard_name(ind1);
+
+% If the dataset is quantitative:
+panavas_nagy_2005.orfs = hit_strains;
+panavas_nagy_2005.ph = hit_data_names;
+panavas_nagy_2005.data = hit_data;
+panavas_nagy_2005.dataset_ids = hit_data_ids;
+
+%% Save
+
+save('./panavas_nagy_2005.mat','panavas_nagy_2005');
+
+%% Print out
+
+fid = fopen('./panavas_nagy_2005.txt','w');
+write_matrix_file(fid, panavas_nagy_2005.orfs, panavas_nagy_2005.ph, panavas_nagy_2005.data);
+fclose(fid);
+
+%% Save to DB (admin)
+
+addpath(genpath('../../Private-Utils/'));
+if exist('save_data_to_db.m')
+    res = save_data_to_db(panavas_nagy_2005)
+end
+
+end
+
