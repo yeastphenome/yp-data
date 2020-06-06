94df4636291a09feefd410d215a59465d1aa1fd9
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-06-05 17:45:47 -0700

    Consolidation of tested strains usage

--- Datasets/23157175/code.py
+++ Datasets/23157175/code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import numpy as np
@@ -16,22 +16,28 @@ from Conversions.translate import *
 from Strings.is_a import *
 
 
+# In[4]:
+
+
+get_ipython().system('pwd')
+
+
 # # Initial setup
 
-# In[2]:
+# In[5]:
 
 
 paper_pmid = 23157175
 paper_name = 'zhang_jiang_2013' 
 
 
-# In[98]:
+# In[6]:
 
 
 datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_list.txt', sep='\t', header=None, names=['pmid', 'name'])
 
 
-# In[99]:
+# In[7]:
 
 
 datasets.set_index('pmid', inplace=True)
@@ -39,55 +45,49 @@ datasets.set_index('pmid', inplace=True)
 
 # # Load & process the data
 
-# In[45]:
+# In[8]:
 
 
 original_data1 = pd.read_excel('raw_data/hits.xlsx', sheet_name='DMSO4')
 original_data2 = pd.read_excel('raw_data/hits.xlsx', sheet_name='DMSO8')
 
 
-# In[46]:
+# In[9]:
 
 
 original_data1.columns = [x.strip() for x in original_data1.columns]
 original_data2.columns = [x.strip() for x in original_data2.columns]
 
 
-# In[47]:
-
-
-original_data2.head()
-
-
-# In[48]:
+# In[11]:
 
 
 original_data1.drop(index=original_data1.loc[original_data1['DMSO sensitivity'].isnull()].index, inplace=True)
 original_data2.drop(index=original_data2.loc[original_data2['8% DMSO sensitivity'].isnull()].index, inplace=True)
 
 
-# In[49]:
+# In[12]:
 
 
 original_data1['DMSO sensitivity'] = original_data1['DMSO sensitivity'].apply(lambda x: len(x.strip()))
 original_data2['8% DMSO sensitivity'] = original_data2['8% DMSO sensitivity'].apply(lambda x: len(x.strip()))
 
 
-# In[50]:
+# In[13]:
 
 
 print('Original data dimensions: %d x %d' % (original_data1.shape))
 print('Original data dimensions: %d x %d' % (original_data2.shape))
 
 
-# In[51]:
+# In[14]:
 
 
 original_data1['genes'] = original_data1['Gene'].astype(str)
 original_data2['genes'] = original_data2['Gene'].astype(str)
 
 
-# In[52]:
+# In[15]:
 
 
 # Eliminate all white spaces & capitalize
@@ -95,7 +95,7 @@ original_data1['genes'] = clean_genename(original_data1['genes'])
 original_data2['genes'] = clean_genename(original_data2['genes'])
 
 
-# In[70]:
+# In[16]:
 
 
 # Translate to ORFs 
@@ -103,7 +103,7 @@ original_data1['orfs'] = translate_sc(original_data1['genes'].values, to='orf')
 original_data2['orfs'] = translate_sc(original_data2['genes'].values, to='orf')
 
 
-# In[71]:
+# In[17]:
 
 
 # Make sure everything translated ok
@@ -111,7 +111,7 @@ t = looks_like_orf(original_data1['orfs'])
 print(original_data1.loc[~t,])
 
 
-# In[72]:
+# In[18]:
 
 
 # Make sure everything translated ok
@@ -119,113 +119,107 @@ t = looks_like_orf(original_data2['orfs'])
 print(original_data2.loc[~t,])
 
 
-# In[73]:
+# In[19]:
 
 
 original_data1['data'] = -original_data1['DMSO sensitivity']
 original_data2['data'] = -original_data2['8% DMSO sensitivity']
 
 
-# In[74]:
+# In[20]:
 
 
 original_data1.set_index('orfs', inplace=True)
 original_data2.set_index('orfs', inplace=True)
 
 
+# In[37]:
+
+
+dataset_ids = [16459, 16460]
+datasets = datasets.reindex(index=dataset_ids)
+
+
+# In[29]:
+
+
+original_data = original_data1['data'].to_frame().join(original_data2['data'], how='outer', lsuffix='_1', rsuffix='_2')
+
+
+# In[31]:
+
+
+original_data.columns = dataset_ids
+
+
+# In[33]:
+
+
+original_data[original_data.isnull()] = 0
+
+
 # # Load & process tested strains
 
-# In[87]:
+# In[35]:
 
 
 tested = pd.read_excel('raw_data/DELETION LIBRARY.xlsx', sheet_name='DELETION LIBRARY', skiprows=1)
-
-
-# In[88]:
-
-
 tested = np.array(tested['ORF name'].unique())
-
-
-# In[89]:
-
-
 tested = clean_orf(tested)
-
-
-# In[90]:
-
-
 tested = translate_sc(tested, to='orf')
-
-
-# In[84]:
-
-
 tested[tested == 'YELOO1C'] = 'YEL001C'
-
-
-# In[85]:
-
-
 # Make sure everything translated ok
 t = looks_like_orf(tested)
 print(tested[~np.array(t)])
-
-
-# In[91]:
-
-
 tested = np.setdiff1d(tested, np.array(['YMR41W']))
+
+
+# In[36]:
+
+
+tested = pd.DataFrame(index=tested)
 
 
 # # Prepare the final dataset
 
-# In[100]:
+# In[38]:
 
 
-dataset_ids = [16459, 16460]
+data = tested.join(original_data, how='outer')
 
 
-# In[101]:
+# In[40]:
 
 
-datasets = datasets.reindex(index=dataset_ids)
+data.columns = datasets.loc[data.columns,'name']
 
 
-# In[102]:
+# In[44]:
 
 
-data = pd.DataFrame(index=tested, columns=datasets['name'].values, data=0)
+data[data.isnull()] = 0
 
 
-# In[103]:
-
-
-data.loc[original_data1.index, datasets['name'].values[0]] = original_data1['data']
-data.loc[original_data2.index, datasets['name'].values[1]] = original_data2['data']
-
-
-# In[104]:
+# In[45]:
 
 
 data = data.groupby(data.index).mean()
 
 
-# In[105]:
+# In[46]:
 
 
 # Create row index
 data.index.name='orf'
 
 
-# In[106]:
+# In[47]:
 
 
 print('Final data dimensions: %d x %d' % (data.shape))
 
 
-# In[108]:
+# In[48]:
 
 
 print((data<0).sum(axis=0))
@@ -233,7 +227,7 @@ print((data<0).sum(axis=0))
 
 # # Print out
 
-# In[109]:
+# In[49]:
 
 
 data.to_csv(paper_name + '.txt', sep='\t')
@@ -241,13 +235,13 @@ data.to_csv(paper_name + '.txt', sep='\t')
 
 # # Save to DB
 
-# In[110]:
+# In[50]:
 
 
 from IO.save_data_to_db2 import *
 
 
-# In[111]:
+# In[51]:
 
 
 # Create column index
@@ -257,7 +251,7 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
 data.columns = idx
 
 
-# In[112]:
+# In[52]:
 
 
 save_data_to_db(data, paper_pmid)
