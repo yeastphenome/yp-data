c7af13f1acd901ec64b8f51e20a7c33c56e16e37
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-07-17 14:16:15 -0700

    New code & data

---
+++ Datasets/11907261/code.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+import sys
+
+from os.path import expanduser
+sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
+
+from Conversions.translate import *
+from Strings.is_a import *
+
+
+# # Initial setup
+
+# In[2]:
+
+
+paper_pmid = 11907261
+paper_name = 'seeley_eitzen_2002' 
+
+
+# In[4]:
+
+
+datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_list.txt', sep='\t', header=None, names=['pmid', 'name'])
+
+
+# In[5]:
+
+
+datasets.set_index('pmid', inplace=True)
+
+
+# # Load & process the data
+
+# In[6]:
+
+
+original_data = pd.read_excel('raw_data/data.xlsx', sheet_name='Sheet1')
+
+
+# In[7]:
+
+
+print('Original data dimensions: %d x %d' % (original_data.shape))
+
+
+# In[8]:
+
+
+original_data['ORF'] = original_data['ORF'].astype(str)
+
+
+# In[9]:
+
+
+# Eliminate all white spaces & capitalize
+original_data['ORF'] = clean_orf(original_data['ORF'])
+
+
+# In[10]:
+
+
+# Translate to ORFs 
+original_data['ORF'] = translate_sc(original_data['ORF'], to='orf')
+
+
+# In[11]:
+
+
+# Make sure everything translated ok
+t = looks_like_orf(original_data['ORF'])
+print(original_data.loc[~t,])
+
+
+# In[13]:
+
+
+original_data.set_index('ORF', inplace=True)
+
+
+# In[15]:
+
+
+original_data[original_data.isnull()] = 0
+
+
+# # Prepare the final dataset
+
+# In[17]:
+
+
+dataset_ids = [16542, 16539, 16540, 16541]
+
+
+# In[18]:
+
+
+datasets = datasets.reindex(index=dataset_ids)
+
+
+# In[19]:
+
+
+data = original_data.copy()
+data.columns = datasets['name'].values
+
+
+# In[20]:
+
+
+data = data.groupby(data.index).mean()
+
+
+# In[21]:
+
+
+# Create row index
+data.index.name='orf'
+
+
+# In[22]:
+
+
+print('Final data dimensions: %d x %d' % (data.shape))
+
+
+# # Print out
+
+# In[24]:
+
+
+data.to_csv(paper_name + '.txt', sep='\t')
+
+
+# # Save to DB
+
+# In[27]:
+
+
+from IO.save_data_to_db2 import *
+
+
+# In[28]:
+
+
+# Create column index
+lst = [datasets.index.values, datasets['name'].values]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
+data.columns = idx
+
+
+# In[29]:
+
+
+save_data_to_db(data, paper_pmid)
+
+
+# In[ ]:
+
+
+
+
