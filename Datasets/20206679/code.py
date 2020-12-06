905c3916c407c43e20e113de2d6795393beb6de0
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-12-06 09:30:35 -0800

    Re-ran code after updating the dataset names

--- Datasets/20206679/code.py
+++ Datasets/20206679/code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
@@ -18,20 +18,20 @@ from Strings.is_a import *
 
 # # Initial setup
 
-# In[3]:
+# In[2]:
 
 
 paper_pmid = 20206679
 paper_name = 'zhao_jiang_2010' 
 
 
-# In[4]:
+# In[3]:
 
 
 datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_list.txt', sep='\t', header=None, names=['pmid', 'name'])
 
 
-# In[5]:
+# In[4]:
 
 
 datasets.set_index('pmid', inplace=True)
@@ -39,58 +39,58 @@ datasets.set_index('pmid', inplace=True)
 
 # # Load & process the data
 
-# In[15]:
+# In[5]:
 
 
 original_data = pd.read_excel('raw_data/hits.xlsx', sheet_name='Sheet1')
 
 
-# In[16]:
+# In[6]:
 
 
 original_data.columns = [x.strip() for x in original_data.columns]
 
 
-# In[17]:
+# In[7]:
 
 
 original_data.head()
 
 
-# In[18]:
+# In[8]:
 
 
 cols = ['Systemic Name','Systemic Name.1','Systemic Name.2','Systemic Name.3']
 original_data = pd.concat([original_data[col] for col in cols], axis=0).to_frame()
 
 
-# In[20]:
+# In[9]:
 
 
 print('Original data dimensions: %d x %d' % (original_data.shape))
 
 
-# In[23]:
+# In[10]:
 
 
 original_data[0] = original_data[0].astype(str)
 
 
-# In[25]:
+# In[11]:
 
 
 # Eliminate all white spaces & capitalize
 original_data['orfs'] = clean_orf(original_data[0])
 
 
-# In[26]:
+# In[12]:
 
 
 # Translate to ORFs 
 original_data['orfs'] = translate_sc(original_data['orfs'].values, to='orf')
 
 
-# In[28]:
+# In[13]:
 
 
 # Make sure everything translated ok
@@ -98,19 +98,19 @@ t = looks_like_orf(original_data['orfs'])
 print(original_data.loc[~t,])
 
 
-# In[29]:
+# In[14]:
 
 
 original_data.drop(index=original_data.loc[~t,].index, inplace=True)
 
 
-# In[30]:
+# In[15]:
 
 
 original_data['data'] = -1
 
 
-# In[31]:
+# In[16]:
 
 
 original_data.set_index('orfs', inplace=True)
@@ -118,7 +118,7 @@ original_data.set_index('orfs', inplace=True)
 
 # # Load & process tested strains
 
-# In[33]:
+# In[17]:
 
 
 tested = pd.read_excel('raw_data/DELETION LIBRARY.xlsx', sheet_name='DELETION LIBRARY', skiprows=1)
@@ -132,7 +132,7 @@ print(tested[~np.array(t)])
 tested = np.setdiff1d(tested, np.array(['YMR41W']))
 
 
-# In[37]:
+# In[18]:
 
 
 tested = pd.DataFrame(index=tested)
@@ -140,51 +140,56 @@ tested = pd.DataFrame(index=tested)
 
 # # Prepare the final dataset
 
-# In[34]:
+# In[19]:
 
 
 dataset_ids = [16456]
 
 
-# In[35]:
+# In[20]:
 
 
 datasets = datasets.reindex(index=dataset_ids)
 
 
-# In[102]:
+# In[21]:
 
 
-data = pd.DataFrame(index=tested, columns=datasets['name'].values, data=0)
+data = tested.join(original_data['data'], how='outer')
 
 
-# In[103]:
+# In[22]:
 
 
-data.loc[original_data1.index, datasets['name'].values[0]] = original_data1['data']
-data.loc[original_data2.index, datasets['name'].values[1]] = original_data2['data']
+data[data['data'].isnull()] = 0
 
 
-# In[104]:
+# In[23]:
 
 
 data = data.groupby(data.index).mean()
 
 
-# In[105]:
+# In[24]:
 
 
 # Create row index
 data.index.name='orf'
 
 
-# In[106]:
+# In[25]:
+
+
+data.columns = datasets['name'].values
+
+
+# In[26]:
 
 
 print('Final data dimensions: %d x %d' % (data.shape))
 
 
-# In[108]:
+# In[27]:
 
 
 print((data<0).sum(axis=0))
@@ -192,7 +197,7 @@ print((data<0).sum(axis=0))
 
 # # Print out
 
-# In[109]:
+# In[28]:
 
 
 data.to_csv(paper_name + '.txt', sep='\t')
@@ -200,13 +205,13 @@ data.to_csv(paper_name + '.txt', sep='\t')
 
 # # Save to DB
 
-# In[110]:
+# In[51]:
 
 
 from IO.save_data_to_db2 import *
 
 
-# In[111]:
+# In[52]:
 
 
 # Create column index
@@ -216,7 +221,7 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
 data.columns = idx
 
 
-# In[112]:
+# In[53]:
 
 
 save_data_to_db(data, paper_pmid)
