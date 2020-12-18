7db01482c3a1b48d1b77fadd99fb7af902db7684
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-12-17 17:41:01 -0800

    Re-ran code with new conventions

--- Datasets/32265288/code.py
+++ Datasets/32265288/code.py
@@ -4,17 +4,9 @@
 # In[1]:
 
 
-import numpy as np
-import pandas as pd
+get_ipython().run_line_magic('run', '../yp_utils.py')
 
-import sys
 import regex
-
-from os.path import expanduser
-sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
-
-from Conversions.translate import *
-from Strings.is_a import *
 
 
 # # Initial setup
@@ -29,13 +21,13 @@ paper_name = 'novarina_chang_2020'
 # In[3]:
 
 
-datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_list.txt', sep='\t', header=None, names=['pmid', 'name'])
+datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_list.txt', sep='\t', header=None, names=['dataset_id', 'name'])
 
 
 # In[4]:
 
 
-datasets.set_index('pmid', inplace=True)
+datasets.set_index('dataset_id', inplace=True)
 
 
 # # Load & process the data
@@ -113,7 +105,7 @@ original_data1['data'] = 1
 original_data1.set_index('orfs', inplace=True)
 
 
-# In[49]:
+# In[16]:
 
 
 original_data1 = original_data1.groupby(original_data1.index).mean()
@@ -121,39 +113,39 @@ original_data1 = original_data1.groupby(original_data1.index).mean()
 
 # ### Dataset 2
 
-# In[16]:
+# In[17]:
 
 
 orf_col = 'ORF name'
 
 
-# In[17]:
+# In[18]:
 
 
 original_data2['orfs'] = original_data2[orf_col].astype(str)
 
 
-# In[18]:
+# In[19]:
 
 
 # Eliminate all white spaces & capitalize
 original_data2['orfs'] = clean_orf(original_data2['orfs'])
 
 
-# In[19]:
+# In[20]:
 
 
 # Translate to ORFs 
 original_data2['orfs'] = translate_sc(original_data2['orfs'], to='orf')
 
 
-# In[20]:
+# In[21]:
 
 
 original_data2.loc[original_data2['orfs']=='YLR287-A','orfs'] = 'YLR287C-A'
 
 
-# In[21]:
+# In[22]:
 
 
 # Make sure everything translated ok
@@ -161,20 +153,20 @@ t = looks_like_orf(original_data2['orfs'])
 print(original_data2.loc[~t,])
 
 
-# In[22]:
+# In[23]:
 
 
 wt_phenotype = 56 # from main text
 original_data2['data'] = (original_data2['Percent recombinants'] / wt_phenotype) - 1
 
 
-# In[23]:
+# In[24]:
 
 
 original_data2.set_index('orfs', inplace=True)
 
 
-# In[50]:
+# In[25]:
 
 
 original_data2 = original_data2.groupby(original_data2.index).mean()
@@ -182,26 +174,26 @@ original_data2 = original_data2.groupby(original_data2.index).mean()
 
 # ### Join the 2 datasets
 
-# In[51]:
+# In[26]:
 
 
 original_data = original_data1[['data']].join(original_data2[['data']], how='outer', lsuffix='_1', rsuffix='_2')
 
 
-# In[52]:
+# In[27]:
 
 
 original_data.shape
 
 
-# In[53]:
+# In[28]:
 
 
 # Orfs from dataset 1 that were not present in dataset 2
 unmatched_orfs = original_data.loc[original_data['data_2'].isnull()].index.values
 
 
-# In[54]:
+# In[29]:
 
 
 # Check for partial matches of hits from dataset1 in the list from dataset2 (possible typos in dataset1)
@@ -216,14 +208,14 @@ for orf in unmatched_orfs:
     print('%s\t%d' % (orf, nm))
 
 
-# In[55]:
+# In[30]:
 
 
 # No obvious typo fixes. Decided to leave dataset 2 values for unmatched orfs at NaN. But use dataset 2 list as the tested universe for dataset 1 (best approximation).
 original_data['data_1'].loc[original_data['data_1'].isnull()] = 0
 
 
-# In[56]:
+# In[31]:
 
 
 original_data.notnull().sum(axis=0)
@@ -231,79 +223,133 @@ original_data.notnull().sum(axis=0)
 
 # # Prepare the final dataset
 
-# In[57]:
+# In[32]:
 
 
 dataset_ids = [16617, 16618]
 
 
-# In[58]:
+# In[33]:
 
 
 datasets = datasets.reindex(index=dataset_ids)
 
 
-# In[59]:
+# In[34]:
 
 
 data = original_data[['data_1','data_2']].copy()
 
 
-# In[60]:
+# In[35]:
 
 
-data.columns = datasets['name'].values
+lst = [datasets.index.values, ['value']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data.columns = idx
 
 
-# In[61]:
+# In[36]:
 
 
 data = data.groupby(data.index).mean()
 
 
-# In[62]:
+# In[37]:
 
 
 # Create row index
 data.index.name='orf'
 
 
-# In[63]:
+# In[38]:
 
 
 print('Final data dimensions: %d x %d' % (data.shape))
 
 
+# ## Subset to the genes currently in SGD
+
+# In[39]:
+
+
+genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
+genes = genes.reset_index().set_index('systematic_name')
+gene_ids = genes.reindex(index=data.index.values)['id'].values
+num_missing = np.sum(np.isnan(gene_ids))
+print('ORFs missing from SGD: %d' % num_missing)
+
+
+# In[40]:
+
+
+data['gene_id'] = gene_ids
+data = data.loc[data['gene_id'].notnull()]
+data['gene_id'] = data['gene_id'].astype(int)
+data = data.reset_index().set_index(['gene_id','orf'])
+
+
+# # Normalize
+
+# In[41]:
+
+
+data_norm = normalize_phenotypic_scores(data, has_tested=True)
+
+
+# In[42]:
+
+
+# Assign proper column names
+lst = [datasets.index.values, ['valuez']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data_norm.columns = idx
+
+
+# In[43]:
+
+
+data_norm[data.isnull()] = np.nan
+
+
+# In[44]:
+
+
+data_all = data.join(data_norm)
+
+
+# In[45]:
+
+
+data_all.head()
+
+
 # # Print out
 
-# In[64]:
+# In[46]:
 
 
-data.to_csv(paper_name + '.txt', sep='\t')
+for f in ['value','valuez']:
+    df = data_all.xs(f, level='data_type', axis=1).copy()
+    df.columns = datasets['name'].values
+    df = df.droplevel('gene_id', axis=0)
+    df.to_csv(paper_name + '_' + f + '.txt', sep='\t')
 
 
 # # Save to DB
 
-# In[66]:
+# In[47]:
 
 
-from IO.save_data_to_db2 import *
+from IO.save_data_to_db3 import *
 
 
-# In[67]:
+# In[48]:
 
 
-# Create column index
-lst = [datasets.index.values, datasets['name'].values]
-tuples = list(zip(*lst))
-idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
-data.columns = idx
-
-
-# In[68]:
-
-
-save_data_to_db(data, paper_pmid)
+save_data_to_db(data_all, paper_pmid)
 
 
 # In[ ]:
