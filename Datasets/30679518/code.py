e3a7099dcaa1f2f3d738776c7acc8ec575d29aa9
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2020-12-23 21:11:39 -0800

    Re-ran code with new conventions

--- Datasets/30679518/code.py
+++ Datasets/30679518/code.py
@@ -4,16 +4,7 @@
 # In[1]:
 
 
-import numpy as np
-import pandas as pd
-
-import sys
-
-from os.path import expanduser
-sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
-
-from Conversions.translate import *
-from Strings.is_a import *
+get_ipython().run_line_magic('run', '../yp_utils.py')
 
 
 # # Initial setup
@@ -67,124 +58,165 @@ original_data['genes'] = clean_genename(original_data['genes'])
 # In[9]:
 
 
-# # If possible, fix typos, omissions, etc.
-# original_data.loc[original_data['genes'].str.contains('2001-10-01'),'genes'] = 'OCT1'
-# original_data.loc[original_data['genes'].str.contains('YBR160WAS'),'genes'] = 'YBR160W'
-
-
-# In[10]:
-
-
 # Translate to ORFs 
 original_data['orfs'] = translate_sc(original_data['genes'], to='orf')
 
 
-# In[11]:
+# In[10]:
 
 
 # Make sure everything translated ok
 t = looks_like_orf(original_data['orfs'])
 
 
-# In[12]:
+# In[11]:
 
 
 print(original_data.loc[~t,])
 
 
-# In[13]:
+# In[12]:
 
 
 to_remove = original_data.loc[original_data['genes'] == '37165',:].index.values
 original_data.drop(index=to_remove, inplace=True)
 
 
-# In[14]:
+# In[13]:
 
 
 original_data.shape
 
 
-# In[22]:
+# In[14]:
 
 
 # If the same strain is present more than once, average its values
 data = original_data.groupby('orfs')['logFC'].mean().to_frame()
 
 
-# In[23]:
+# In[15]:
 
 
 print('Final data dimensions: %d x %d' % (data.shape))
+
+
+# In[17]:
+
+
+data.index.name='orf'
 
 
 # # Prepare the final dataset
 
-# In[24]:
+# In[18]:
 
 
 dataset_ids = [16440]
-
-
-# In[25]:
-
-
 datasets = datasets.reindex(index=dataset_ids)
 
 
-# In[27]:
+# In[19]:
 
 
-data.columns = datasets['name'].values
+lst = [datasets.index.values, ['value']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data.columns = idx
 
 
-# In[28]:
+# In[20]:
 
 
-data = data.groupby(data.index).mean()
+data.head()
 
 
-# In[29]:
-
-
-# Create row index
-data.index.name='orf'
-
-
-# In[30]:
-
-
-print('Final data dimensions: %d x %d' % (data.shape))
-
-
-# # Print out
-
-# In[31]:
-
-
-data.to_csv(paper_name + '.txt', sep='\t')
-
-
-# # Save to DB
+# ## Subset to the genes currently in SGD
 
 # In[21]:
 
 
-from IO.save_data_to_db2 import *
-
-
-# In[ ]:
-
-
-# Create column index
-lst = [datasets.index.values, datasets['name'].values]
-tuples = list(zip(*lst))
-idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
-data.columns = idx
+genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
+genes = genes.reset_index().set_index('systematic_name')
+gene_ids = genes.reindex(index=data.index.values)['id'].values
+num_missing = np.sum(np.isnan(gene_ids))
+print('ORFs missing from SGD: %d' % num_missing)
 
 
 # In[22]:
 
 
-save_data_to_db(data, paper_pmid)
+data['gene_id'] = gene_ids
+data = data.loc[data['gene_id'].notnull()]
+data['gene_id'] = data['gene_id'].astype(int)
+data = data.reset_index().set_index(['gene_id','orf'])
+
+
+# In[23]:
+
+
+data.head()
+
+
+# # Normalize
+
+# In[24]:
+
+
+data_norm = normalize_phenotypic_scores(data, has_tested=True)
+
+
+# In[25]:
+
+
+# Assign proper column names
+lst = [datasets.index.values, ['valuez']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data_norm.columns = idx
+
+
+# In[26]:
+
+
+data_norm[data.isnull()] = np.nan
+data_all = data.join(data_norm)
+
+
+# In[27]:
+
+
+data_all.head()
+
+
+# # Print out
+
+# In[28]:
+
+
+for f in ['value','valuez']:
+    df = data_all.xs(f, level='data_type', axis=1).copy()
+    df.columns = datasets['name'].values
+    df = df.droplevel('gene_id', axis=0)
+    df.to_csv(paper_name + '_' + f + '.txt', sep='\t')
+
+
+# # Save to DB
+
+# In[29]:
+
+
+from IO.save_data_to_db3 import *
+
+
+# In[30]:
+
+
+save_data_to_db(data_all, paper_pmid)
+
+
+# In[ ]:
+
+
+
 
