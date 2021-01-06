0e2560c2585bf9190f66868c105b4b56e602b5b2
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2021-01-05 21:30:29 -0800

    Re-ran with new conventions

--- Datasets/23335509/code.py
+++ Datasets/23335509/code.py
@@ -4,17 +4,7 @@
 # In[1]:
 
 
-import numpy as np
-import pandas as pd
-
-import sys
-
-from os.path import expanduser
-sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
-
-from Conversions.translate import *
-from Strings.is_a import *
-from Math.normalize import z_transform_mode
+get_ipython().run_line_magic('run', '../yp_utils.py')
 
 
 # # Initial setup
@@ -38,34 +28,27 @@ datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_lis
 datasets.set_index('pmid', inplace=True)
 
 
-# In[5]:
-
-
-path_to_genes = '../../Private-Utils/datasets_gene2.txt'
-path_to_consensus_tested = '../../Private-Utils/yp_2020-09-01_orfs.txt'
-
-
 # # Load & process the data
 
-# In[26]:
+# In[5]:
 
 
 original_data = pd.read_excel('raw_data/jbc.M112.425769-1.xlsx', sheet_name='Sheet1')
 
 
-# In[27]:
+# In[6]:
 
 
 print('Original data dimensions: %d x %d' % (original_data.shape))
 
 
-# In[28]:
+# In[7]:
 
 
 original_data['genes'] = original_data['Gene'].astype(str)
 
 
-# In[29]:
+# In[8]:
 
 
 original_data['genes2'] = original_data['genes']
@@ -76,21 +59,21 @@ for i, r in original_data.loc[ix,].iterrows():
     original_data.at[i,'genes2'] = t[0]
 
 
-# In[30]:
+# In[9]:
 
 
 # Eliminate all white spaces & capitalize
 original_data['genes2'] = clean_genename(original_data['genes2'])
 
 
-# In[31]:
+# In[10]:
 
 
 # Translate to ORFs 
 original_data['orfs'] = translate_sc(original_data['genes2'], to='orf')
 
 
-# In[32]:
+# In[11]:
 
 
 # Make sure everything translated ok
@@ -98,26 +81,26 @@ t = looks_like_orf(original_data['orfs'])
 print(original_data.loc[~t,])
 
 
-# In[33]:
+# In[12]:
 
 
 original_data['data'] = original_data['Resistance'].apply(lambda x: len(x))
 
 
-# In[38]:
+# In[13]:
 
 
 original_data.set_index('orfs', inplace=True)
 original_data.index.name='orf'
 
 
-# In[39]:
+# In[14]:
 
 
 original_data = original_data.groupby(original_data.index).mean()
 
 
-# In[40]:
+# In[15]:
 
 
 original_data.shape
@@ -125,20 +108,20 @@ original_data.shape
 
 # # Prepare the final dataset
 
-# In[41]:
+# In[16]:
 
 
 data = original_data[['data']].copy()
 
 
-# In[42]:
+# In[17]:
 
 
 dataset_ids = [16563]
 datasets = datasets.reindex(index=dataset_ids)
 
 
-# In[43]:
+# In[18]:
 
 
 lst = [datasets.index.values, ['value']*datasets.shape[0]]
@@ -147,7 +130,7 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
 data.columns = idx
 
 
-# In[44]:
+# In[19]:
 
 
 data.head()
@@ -155,7 +138,7 @@ data.head()
 
 # ## Subset to the genes currently in SGD
 
-# In[45]:
+# In[20]:
 
 
 genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
@@ -165,7 +148,7 @@ num_missing = np.sum(np.isnan(gene_ids))
 print('ORFs missing from SGD: %d' % num_missing)
 
 
-# In[46]:
+# In[21]:
 
 
 data['gene_id'] = gene_ids
@@ -174,7 +157,7 @@ data['gene_id'] = data['gene_id'].astype(int)
 data = data.reset_index().set_index(['gene_id','orf'])
 
 
-# In[47]:
+# In[22]:
 
 
 data.head()
@@ -183,38 +166,13 @@ data.head()
 # # Normalize
 # 
 
-# In[55]:
-
-
-def normalize_phenotypic_scores(df, has_tested=False):
-    
-    if not has_tested:
-        
-        genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
-        genes = genes.reset_index().set_index('systematic_name', drop=False)
-        
-        yp_orfs = pd.read_csv(path_to_consensus_tested, header=None)
-        yp_orfs = yp_orfs[1].values
-        consensus_tested = [tuple(genes.loc[orf,['id','systematic_name']]) for orf in yp_orfs]
-        
-        df_index = [tuple(x) for x in df.index]
-        
-        consensus_tested = list(set(consensus_tested + df_index))
-        
-        df = df.reindex(index=consensus_tested, fill_value=0)
-        
-    df_norm = z_transform_mode(df)
-    
-    return df_norm
-
-
-# In[56]:
+# In[23]:
 
 
 data_norm = normalize_phenotypic_scores(data, has_tested=False)
 
 
-# In[57]:
+# In[24]:
 
 
 # Assign proper column names
@@ -224,19 +182,19 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
 data_norm.columns = idx
 
 
-# In[58]:
+# In[25]:
 
 
 data_norm[data.isnull()] = np.nan
 
 
-# In[59]:
+# In[26]:
 
 
 data_all = data.join(data_norm)
 
 
-# In[60]:
+# In[27]:
 
 
 data_all.head()
@@ -244,7 +202,7 @@ data_all.head()
 
 # # Print out
 
-# In[62]:
+# In[28]:
 
 
 for f in ['value','valuez']:
@@ -256,13 +214,13 @@ for f in ['value','valuez']:
 
 # # Save to DB
 
-# In[63]:
+# In[29]:
 
 
 from IO.save_data_to_db3 import *
 
 
-# In[64]:
+# In[30]:
 
 
 save_data_to_db(data_all, paper_pmid)
