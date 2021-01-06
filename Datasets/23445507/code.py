0987ecd0a94c97f4cbf044b594d5f5e8b1e62a2a
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2021-01-05 20:57:28 -0800

    Re-ran with new conventions

--- Datasets/23445507/code.py
+++ Datasets/23445507/code.py
@@ -4,16 +4,7 @@
 # In[1]:
 
 
-import numpy as np
-import pandas as pd
-
-import sys
-
-from os.path import expanduser
-sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
-
-from Conversions.translate import *
-from Strings.is_a import *
+get_ipython().run_line_magic('run', '../yp_utils.py')
 
 
 # # Initial setup
@@ -71,14 +62,14 @@ original_data['orf'] = clean_orf(original_data['orf'])
 original_data['orf'] = translate_sc(original_data['orf'], to='orf')
 
 
-# In[11]:
+# In[10]:
 
 
 original_data.loc[original_data['orf']=='YDR369', 'orf'] = 'YDR369C'
 original_data.loc[original_data['orf']=='YOR382', 'orf'] = 'YOR382W'
 
 
-# In[12]:
+# In[11]:
 
 
 # Make sure everything translated ok
@@ -86,105 +77,152 @@ t = looks_like_orf(original_data['orf'])
 print(original_data.loc[~t,])
 
 
-# In[19]:
+# In[12]:
 
 
 data_dict = {'+': 1,'++': 2,'-': -1, 'nd': np.nan,'+/-': 0, '(+)/-': 0}
 
 
-# In[20]:
+# In[13]:
 
 
 original_data['data'] = original_data['120 μg/ml DAPG']
 
 
-# In[21]:
+# In[14]:
 
 
 original_data['data'] = [data_dict[x] for x in original_data['data']]
 
 
-# In[22]:
+# In[15]:
 
 
 original_data.set_index('orf', inplace=True)
 
 
+# In[16]:
+
+
+original_data = original_data[['data']].copy()
+
+
+# In[17]:
+
+
+original_data = original_data.groupby(original_data.index).mean()
+
+
+# In[18]:
+
+
+original_data.shape
+
+
 # # Prepare the final dataset
+
+# In[19]:
+
+
+data = original_data.copy()
+
+
+# In[20]:
+
+
+dataset_ids = [16562]
+datasets = datasets.reindex(index=dataset_ids)
+
+
+# In[21]:
+
+
+lst = [datasets.index.values, ['value']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data.columns = idx
+
+
+# In[22]:
+
+
+data.head()
+
+
+# ## Subset to the genes currently in SGD
+
+# In[23]:
+
+
+genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
+genes = genes.reset_index().set_index('systematic_name')
+gene_ids = genes.reindex(index=data.index.values)['id'].values
+num_missing = np.sum(np.isnan(gene_ids))
+print('ORFs missing from SGD: %d' % num_missing)
+
+
+# In[24]:
+
+
+data['gene_id'] = gene_ids
+data = data.loc[data['gene_id'].notnull()]
+data['gene_id'] = data['gene_id'].astype(int)
+data = data.reset_index().set_index(['gene_id','orf'])
+
+data.head()
+
+
+# # Normalize
 
 # In[25]:
 
 
-dataset_ids = [16562]
+data_norm = normalize_phenotypic_scores(data, has_tested=False)
 
 
 # In[26]:
 
 
-datasets = datasets.reindex(index=dataset_ids)
+# Assign proper column names
+lst = [datasets.index.values, ['valuez']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data_norm.columns = idx
 
 
 # In[27]:
 
 
-data = original_data[['data']].copy()
+data_norm[data.isnull()] = np.nan
+data_all = data.join(data_norm)
 
+data_all.head()
+
+
+# # Print out
 
 # In[28]:
 
 
-data.columns = datasets['name'].values
+for f in ['value','valuez']:
+    df = data_all.xs(f, level='data_type', axis=1).copy()
+    df.columns = datasets['name'].values
+    df = df.droplevel('gene_id', axis=0)
+    df.to_csv(paper_name + '_' + f + '.txt', sep='\t')
 
+
+# # Save to DB
 
 # In[29]:
 
 
-data = data.groupby(data.index).mean()
+from IO.save_data_to_db3 import *
 
 
 # In[30]:
 
 
-# Create row index
-data.index.name='orf'
-
-
-# In[31]:
-
-
-print('Final data dimensions: %d x %d' % (data.shape))
-
-
-# # Print out
-
-# In[32]:
-
-
-data.to_csv(paper_name + '.txt', sep='\t')
-
-
-# # Save to DB
-
-# In[33]:
-
-
-from IO.save_data_to_db2 import *
-
-
-# In[34]:
-
-
-# Create column index
-lst = [datasets.index.values, datasets['name'].values]
-tuples = list(zip(*lst))
-idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
-data.columns = idx
-
-
-# In[35]:
-
-
-save_data_to_db(data, paper_pmid)
+save_data_to_db(data_all, paper_pmid)
 
 
 # In[ ]:
