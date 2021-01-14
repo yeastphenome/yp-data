9284f419a07605a15cd20a23f230aa525a2cc61d
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2021-01-13 21:23:44 -0800

    Re-wrote code in python and re-ran

---
+++ Datasets/19917080/code.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('run', '../yp_utils.py')
+
+
+# # Initial setup
+
+# In[2]:
+
+
+paper_pmid = 19917080
+paper_name = 'arita_costa_2009' 
+
+
+# In[3]:
+
+
+datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_list.txt', sep='\t', header=None, names=['dataset_id', 'name'])
+
+
+# In[4]:
+
+
+datasets.set_index('dataset_id', inplace=True)
+
+
+# # Load & process the data
+
+# In[23]:
+
+
+original_data = pd.read_excel('raw_data/1471-2164-10-524-S1.XLS', sheet_name='Sheet1', skiprows=5)
+
+
+# In[24]:
+
+
+print('Original data dimensions: %d x %d' % (original_data.shape))
+
+
+# In[25]:
+
+
+original_data.head()
+
+
+# In[26]:
+
+
+original_data['data1'] = -1
+original_data['data2'] = 1
+
+
+# In[27]:
+
+
+original_data1 = original_data[['sensitive strains','data1']].copy()
+original_data2 = original_data[['resistant strains','data2']].copy()
+
+original_data1.columns=['orf','data']
+original_data2.columns=['orf','data']
+
+
+# In[28]:
+
+
+original_data = pd.concat((original_data1, original_data2), axis=0)
+
+
+# In[31]:
+
+
+original_data['orf'] = original_data['orf'].astype(str)
+
+
+# In[32]:
+
+
+# Eliminate all white spaces & capitalize
+original_data['orf'] = clean_orf(original_data['orf'])
+
+
+# In[34]:
+
+
+# Translate to ORFs 
+original_data['orf'] = translate_sc(original_data['orf'].values, to='orf')
+
+
+# In[35]:
+
+
+# Make sure everything translated ok
+t = looks_like_orf(original_data['orf'])
+print(original_data.loc[~t,])
+
+
+# In[36]:
+
+
+original_data = original_data.loc[t,:]
+
+
+# In[37]:
+
+
+original_data.set_index('orf', inplace=True)
+
+
+# In[38]:
+
+
+original_data = original_data[['data']].copy()
+
+
+# In[39]:
+
+
+original_data = original_data.groupby(original_data.index).mean()
+
+
+# In[40]:
+
+
+original_data.shape
+
+
+# # Prepare the final dataset
+
+# In[41]:
+
+
+data = original_data.copy()
+
+
+# In[42]:
+
+
+dataset_ids = [163]
+datasets = datasets.reindex(index=dataset_ids)
+
+
+# In[43]:
+
+
+lst = [datasets.index.values, ['value']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data.columns = idx
+
+
+# In[44]:
+
+
+data.head()
+
+
+# ## Subset to the genes currently in SGD
+
+# In[45]:
+
+
+genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
+genes = genes.reset_index().set_index('systematic_name')
+gene_ids = genes.reindex(index=data.index.values)['id'].values
+num_missing = np.sum(np.isnan(gene_ids))
+print('ORFs missing from SGD: %d' % num_missing)
+
+
+# In[46]:
+
+
+data['gene_id'] = gene_ids
+data = data.loc[data['gene_id'].notnull()]
+data['gene_id'] = data['gene_id'].astype(int)
+data = data.reset_index().set_index(['gene_id','orf'])
+
+data.head()
+
+
+# # Normalize
+
+# In[47]:
+
+
+data_norm = normalize_phenotypic_scores(data, has_tested=False)
+
+
+# In[48]:
+
+
+# Assign proper column names
+lst = [datasets.index.values, ['valuez']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data_norm.columns = idx
+
+
+# In[49]:
+
+
+data_norm[data.isnull()] = np.nan
+data_all = data.join(data_norm)
+
+data_all.head()
+
+
+# # Print out
+
+# In[50]:
+
+
+for f in ['value','valuez']:
+    df = data_all.xs(f, level='data_type', axis=1).copy()
+    df.columns = datasets['name'].values
+    df = df.droplevel('gene_id', axis=0)
+    df.to_csv(paper_name + '_' + f + '.txt', sep='\t')
+
+
+# # Save to DB
+
+# In[51]:
+
+
+from IO.save_data_to_db3 import *
+
+
+# In[52]:
+
+
+save_data_to_db(data_all, paper_pmid)
+
+
+# In[ ]:
+
+
+
+
