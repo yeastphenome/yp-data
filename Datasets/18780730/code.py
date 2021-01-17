21c87eb3d5636ab48672c06c784dc8f7d9583759
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2021-01-16 19:14:55 -0800

    Re-ran with new conventions

--- Datasets/18780730/code.py
+++ Datasets/18780730/code.py
@@ -4,16 +4,7 @@
 # In[1]:
 
 
-import numpy as np
-import pandas as pd
-
-import sys
-
-from os.path import expanduser
-sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
-
-from Conversions.translate import *
-from Strings.is_a import *
+get_ipython().run_line_magic('run', '../yp_utils.py')
 
 
 # # Initial setup
@@ -121,6 +112,7 @@ original_data['rapa_36h'] = original_data['30C_RAPA_T3'] / original_data['T0']
 
 
 original_data.set_index('orfs', inplace=True)
+original_data.index.name='orf'
 
 
 # In[18]:
@@ -148,80 +140,140 @@ original_data_het.shape
 # In[21]:
 
 
-# Pull them back together
-data = original_data_hom[['37C','rapa_12h','rapa_24h','rapa_36h']].join(original_data_het[['37C','rapa_12h','rapa_24h','rapa_36h']],
-                                                                       lsuffix='_hom', rsuffix='_het', how='outer')
+original_data_hom = original_data_hom[['37C','rapa_12h','rapa_24h','rapa_36h']].copy()
+original_data_het = original_data_het[['37C','rapa_12h','rapa_24h','rapa_36h']].copy()
 
-
-# # Prepare the final dataset
 
 # In[22]:
 
 
-dataset_ids = [16511,16639,16640,16641,16638,16642,16643,16644]
+original_data_hom = original_data_hom.groupby(original_data_hom.index).mean()
+original_data_het = original_data_het.groupby(original_data_het.index).mean()
 
 
 # In[23]:
 
 
-datasets = datasets.reindex(index=dataset_ids)
+# Pull them back together
+original_data = original_data_hom.join(original_data_het, lsuffix='_hom', rsuffix='_het', how='outer')
 
 
 # In[24]:
 
 
-data.columns = datasets['name'].values
+original_data.shape
 
 
 # In[25]:
 
 
-data = data.groupby(data.index).mean()
+original_data.head()
 
+
+# # Prepare the final dataset
 
 # In[26]:
 
 
-# Create row index
-data.index.name='orf'
+data = original_data.copy()
 
 
 # In[27]:
 
 
-print('Final data dimensions: %d x %d' % (data.shape))
+dataset_ids = [16511,16639,16640,16641,16638,16642,16643,16644]
+datasets = datasets.reindex(index=dataset_ids)
 
-
-# # Print out
 
 # In[28]:
 
 
-data.to_csv(paper_name + '.txt', sep='\t')
+lst = [datasets.index.values, ['value']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data.columns = idx
+
+
+# In[29]:
+
+
+data.head()
+
+
+# ## Subset to the genes currently in SGD
+
+# In[30]:
+
+
+genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
+genes = genes.reset_index().set_index('systematic_name')
+gene_ids = genes.reindex(index=data.index.values)['id'].values
+num_missing = np.sum(np.isnan(gene_ids))
+print('ORFs missing from SGD: %d' % num_missing)
+
+
+# In[31]:
+
+
+data['gene_id'] = gene_ids
+data = data.loc[data['gene_id'].notnull()]
+data['gene_id'] = data['gene_id'].astype(int)
+data = data.reset_index().set_index(['gene_id','orf'])
+
+data.head()
+
+
+# # Normalize
+
+# In[32]:
+
+
+data_norm = normalize_phenotypic_scores(data, has_tested=True)
+
+
+# In[33]:
+
+
+# Assign proper column names
+lst = [datasets.index.values, ['valuez']*datasets.shape[0]]
+tuples = list(zip(*lst))
+idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
+data_norm.columns = idx
+
+
+# In[34]:
+
+
+data_norm[data.isnull()] = np.nan
+data_all = data.join(data_norm)
+
+data_all.head()
+
+
+# # Print out
+
+# In[35]:
+
+
+for f in ['value','valuez']:
+    df = data_all.xs(f, level='data_type', axis=1).copy()
+    df.columns = datasets['name'].values
+    df = df.droplevel('gene_id', axis=0)
+    df.to_csv(paper_name + '_' + f + '.txt', sep='\t')
 
 
 # # Save to DB
 
-# In[50]:
+# In[36]:
 
 
-from IO.save_data_to_db2 import *
+from IO.save_data_to_db3 import *
 
 
-# In[52]:
+# In[37]:
 
 
-# Create column index
-lst = [datasets.index.values, datasets['name'].values]
-tuples = list(zip(*lst))
-idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','dataset_name'])
-data.columns = idx
-
-
-# In[53]:
-
-
-save_data_to_db(data, paper_pmid)
+save_data_to_db(data_all, paper_pmid)
 
 
 # In[ ]:
