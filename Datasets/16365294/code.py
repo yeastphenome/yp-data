2758bba98db24cacfdc3963489d80a73949c888a
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2021-01-27 14:41:57 -0800

    Re-ran with new conventions

--- Datasets/16365294/code.py
+++ Datasets/16365294/code.py
@@ -4,18 +4,7 @@
 # In[1]:
 
 
-import numpy as np
-import pandas as pd
-
-import sys
-import re
-
-from os.path import expanduser
-sys.path.append(expanduser('~') + '/Lab/Utils/Python/')
-
-from Conversions.translate import *
-from Strings.is_a import *
-from Math.normalize import z_transform_mode
+get_ipython().run_line_magic('run', '../yp_utils.py')
 
 
 # # Initial setup
@@ -39,54 +28,47 @@ datasets = pd.read_csv('extras/YeastPhenome_' + str(paper_pmid) + '_datasets_lis
 datasets.set_index('pmid', inplace=True)
 
 
-# In[5]:
-
-
-path_to_genes = '../../Private-Utils/datasets_gene2.txt'
-path_to_consensus_tested = '../../Private-Utils/yp_2020-09-01_orfs.txt'
-
-
 # # Load & process the data
 
-# In[6]:
+# In[5]:
 
 
 original_data = pd.read_csv('raw_data/mutant_analysis_2011_10_20.tab', sep='\t')
 
 
-# In[7]:
+# In[6]:
 
 
 original_data.head()
 
 
-# In[8]:
+# In[7]:
 
 
 print('Original data dimensions: %d x %d' % (original_data.shape))
 
 
-# In[9]:
+# In[8]:
 
 
 original_data['name'] = original_data['name'].astype(str)
 
 
-# In[10]:
+# In[9]:
 
 
 # Eliminate all white spaces & capitalize
 original_data['name'] = clean_orf(original_data['name'])
 
 
-# In[11]:
+# In[10]:
 
 
 # Translate to ORFs 
 original_data['name'] = translate_sc(original_data['name'], to='orf')
 
 
-# In[12]:
+# In[11]:
 
 
 # Make sure everything translated ok
@@ -94,11 +76,17 @@ t = looks_like_orf(original_data['name'])
 print(original_data.loc[~t,])
 
 
-# In[13]:
+# In[12]:
 
 
 original_data.set_index('name', inplace=True)
 original_data.index.name='orf'
+
+
+# In[13]:
+
+
+original_data = original_data.apply(pd.to_numeric, axis=1, errors='coerce')
 
 
 # In[14]:
@@ -197,35 +185,10 @@ data.head()
 # In[25]:
 
 
-def normalize_phenotypic_scores(df, has_tested=False):
-    
-    if not has_tested:
-        
-        genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
-        genes = genes.reset_index().set_index('systematic_name', drop=False)
-        
-        yp_orfs = pd.read_csv(path_to_consensus_tested, header=None)
-        yp_orfs = yp_orfs[1].values
-        consensus_tested = [tuple(genes.loc[orf,['id','systematic_name']]) for orf in yp_orfs]
-        
-        df_index = [tuple(x) for x in df.index]
-        
-        consensus_tested = list(set(consensus_tested + df_index))
-        
-        df = df.reindex(index=consensus_tested, fill_value=0)
-        
-    df_norm = z_transform_mode(df)
-    
-    return df_norm
-
-
-# In[26]:
-
-
 data_norm = normalize_phenotypic_scores(data, has_tested=True)
 
 
-# In[27]:
+# In[26]:
 
 
 # Assign proper column names
@@ -235,19 +198,19 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
 data_norm.columns = idx
 
 
-# In[28]:
+# In[27]:
 
 
 data_norm[data.isnull()] = np.nan
 
 
-# In[29]:
+# In[28]:
 
 
 data_all = data.join(data_norm)
 
 
-# In[30]:
+# In[29]:
 
 
 data_all.head()
@@ -255,7 +218,7 @@ data_all.head()
 
 # # Print out
 
-# In[31]:
+# In[30]:
 
 
 for f in ['value','valuez']:
@@ -267,13 +230,13 @@ for f in ['value','valuez']:
 
 # # Save to DB
 
-# In[32]:
+# In[31]:
 
 
 from IO.save_data_to_db3 import *
 
 
-# In[33]:
+# In[32]:
 
 
 save_data_to_db(data_all, paper_pmid)
