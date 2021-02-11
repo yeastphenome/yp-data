162051d02cab14cf4de12e2251d7bf8f021d2538
Anastasia Baryshnikova <abaryshnikova@calicolabs.com>
2021-02-10 20:01:37 -0800

    Fixed bug with flipped dataset ids/names

--- Datasets/31734159/code.py
+++ Datasets/31734159/code.py
@@ -45,60 +45,66 @@ print('Original data dimensions: %d x %d' % (original_data.shape))
 # In[7]:
 
 
-hit_orfs = np.hstack([original_data.iloc[:,c].values for c in [0,5,10,19]])
+original_data.head()
 
 
 # In[8]:
 
 
-hit_orfs.shape
+hit_orfs = np.hstack([original_data.iloc[:,c].values for c in [0,5,10,19]])
 
 
 # In[9]:
 
 
-hit_orfs = hit_orfs.astype(str)
+hit_orfs.shape
 
 
 # In[10]:
+
+
+hit_orfs = hit_orfs.astype(str)
+
+
+# In[11]:
 
 
 hit_data1 = np.hstack([original_data.iloc[:,c].values for c in [1,6,11,20]])
 hit_data2 = np.hstack([original_data.iloc[:,c].values for c in [3,8,13]]+[np.zeros((original_data.shape[0]))+np.nan])
 
 
-# In[11]:
+# In[12]:
 
 
 hit_data1.shape
 
 
-# In[12]:
+# In[13]:
 
 
 hit_data2.shape
 
 
-# In[13]:
+# In[14]:
 
 
 original_data2 = pd.DataFrame(index=hit_orfs, columns=['0_iso','tf'], data=np.vstack((hit_data1, hit_data2)).T)
 
 
-# In[14]:
+# In[15]:
 
 
 original_data2['orfs'] = original_data2.index.values.astype(str)
 
 
-# In[15]:
+# In[16]:
 
 
 # Eliminate all white spaces & capitalize
 original_data2['orfs'] = clean_orf(original_data2['orfs'])
 
 
-# In[16]:
+# In[17]:
 
 
 typo_fix = {'VER093C-A': 'YER093C-A','YMROB4W':'YMR084W','YARD02C-A':'YAR002C-A','YFLOOIW':'YFL001W',
@@ -108,44 +114,44 @@ typo_fix = {'VER093C-A': 'YER093C-A','YMROB4W':'YMR084W','YARD02C-A':'YAR002C-A'
            'YJR044C6':'YJR044C','VCR031C':'YCR031C'}
 
 
-# In[17]:
+# In[18]:
 
 
 original_data2['orfs'] = original_data2['orfs'].apply(lambda x: typo_fix[x] if x in list(typo_fix.keys()) else x)
 
 
-# In[18]:
-
-
-original_data2.shape
-
-
 # In[19]:
 
 
-original_data2 = original_data2.groupby('orfs').mean().reset_index()
+original_data2.shape
 
 
 # In[20]:
 
 
-original_data2.shape
+original_data2 = original_data2.groupby('orfs').mean().reset_index()
 
 
 # In[21]:
 
 
-original_data2.head()
+original_data2.shape
 
 
 # In[22]:
+
+
+original_data2.head()
+
+
+# In[23]:
 
 
 # Translate to ORFs 
 original_data2['orfs'] = translate_sc(original_data2['orfs'], to='orf')
 
 
-# In[23]:
+# In[24]:
 
 
 # Make sure everything translated ok
@@ -153,58 +159,64 @@ t = looks_like_orf(original_data2['orfs'])
 print(original_data2.loc[~t,])
 
 
-# In[24]:
+# In[25]:
 
 
 original_data2 = original_data2.loc[t,:]
 
 
-# In[25]:
-
-
-original_data2.shape
-
-
 # In[26]:
 
 
-original_data2 = original_data2.groupby('orfs').mean()
+original_data2.shape
 
 
 # In[27]:
 
 
-original_data2.head()
+original_data2 = original_data2.groupby('orfs').mean()
 
 
 # In[28]:
 
 
-original_data2.index.name='orf'
+original_data2.head()
 
 
 # In[29]:
 
 
+original_data2.index.name='orf'
+
+
+# In[30]:
+
+
 original_data2.shape
+
+
+# In[37]:
+
+
+original_data2.loc['YBR010W',]
 
 
 # # Prepare the final dataset
 
-# In[30]:
+# In[38]:
 
 
 data = original_data2[['0_iso','tf']].copy()
 
 
-# In[31]:
+# In[39]:
 
 
-dataset_ids = [16411,16414]
+dataset_ids = [16414,16411]
 datasets = datasets.reindex(index=dataset_ids)
 
 
-# In[32]:
+# In[40]:
 
 
 lst = [datasets.index.values, ['value']*datasets.shape[0]]
@@ -213,7 +225,7 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
 data.columns = idx
 
 
-# In[33]:
+# In[41]:
 
 
 data.head()
@@ -221,7 +233,7 @@ data.head()
 
 # ## Subset to the genes currently in SGD
 
-# In[34]:
+# In[42]:
 
 
 genes = pd.read_csv(path_to_genes, sep='\t', index_col='id')
@@ -231,7 +243,7 @@ num_missing = np.sum(np.isnan(gene_ids))
 print('ORFs missing from SGD: %d' % num_missing)
 
 
-# In[35]:
+# In[43]:
 
 
 data['gene_id'] = gene_ids
@@ -240,7 +252,7 @@ data['gene_id'] = data['gene_id'].astype(int)
 data = data.reset_index().set_index(['gene_id','orf'])
 
 
-# In[36]:
+# In[44]:
 
 
 data.head()
@@ -248,13 +260,13 @@ data.head()
 
 # # Normalize
 
-# In[37]:
+# In[45]:
 
 
 data_norm = normalize_phenotypic_scores(data, has_tested=True)
 
 
-# In[38]:
+# In[46]:
 
 
 # Assign proper column names
@@ -264,19 +276,19 @@ idx = pd.MultiIndex.from_tuples(tuples, names=['dataset_id','data_type'])
 data_norm.columns = idx
 
 
-# In[39]:
+# In[47]:
 
 
 data_norm[data.isnull()] = np.nan
 
 
-# In[40]:
+# In[48]:
 
 
 data_all = data.join(data_norm)
 
 
-# In[41]:
+# In[49]:
 
 
 data_all.head()
@@ -284,7 +296,7 @@ data_all.head()
 
 # # Print out
 
-# In[42]:
+# In[50]:
 
 
 for f in ['value','valuez']:
@@ -296,13 +308,13 @@ for f in ['value','valuez']:
 
 # # Save to DB
 
-# In[43]:
+# In[51]:
 
 
 from IO.save_data_to_db3 import *
 
 
-# In[44]:
+# In[52]:
 
 
 save_data_to_db(data_all, paper_pmid)
